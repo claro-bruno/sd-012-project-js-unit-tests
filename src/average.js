205e24151d2e596a2426b3b5346e2183f3cf9b0f
julio11lp <julio11lp@hotmail.com>
2021-05-24 15:31:59 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/average.js
+++ src/average.js
@@ -4,7 +4,7 @@
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
 
-  Parâmetros:
+  Parâmetros
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
     - average([2, 2]) // Retorno: 2;
