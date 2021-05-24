abdeeacf603018943591f5af1b2a86f804a03af7
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-05-24 16:02:55 -0400

    cria a função createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (phrase) => {
+  const estudante = {
+    name: phrase,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
