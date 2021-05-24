1d8a9a7d86aa36b8f7c86b8f0c650adfd910552f
Stefano <stefanooliveira1@gmail.com>
2021-05-24 19:21:14 -0300

    req linter req 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -17,28 +17,8 @@
 const createStudent = (nome) => {
    let student = {
       name: nome,
-      feedback: () => 'Eita pessoa boa!'
+      feedback: () => 'Eita pessoa boa!',
    };
    return student;
 };
-const assert = require('assert');
-const goodPerson = 'Eita pessoa boa!';
-const estudante = createStudent('Leandrão, o Lobo Solitário');
-assert.strictEqual(typeof estudante, 'object');
-assert.strictEqual(typeof estudante.feedback, 'function');
-assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
-assert.strictEqual(estudante.feedback(), goodPerson);
-
-const estudante2 = createStudent('Nobre');
-assert.strictEqual(typeof estudante2, 'object');
-assert.strictEqual(typeof estudante2.feedback, 'function');
-assert.strictEqual(estudante2.name, 'Nobre');
-assert.strictEqual(estudante2.feedback(), goodPerson);
-
-const estudante3 = createStudent('Inácio');
-assert.strictEqual(typeof estudante3, 'object');
-assert.strictEqual(typeof estudante3.feedback, 'function');
-assert.strictEqual(estudante3.name, 'Inácio');
-assert.strictEqual(estudante3.feedback(), goodPerson);
-
 module.exports = createStudent;
