e5c736cc7bce9b74737643adcf645d3234dc5724
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-24 19:09:20 -0300

    Realiza requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,5 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => ({ nome, feedback: () => 'Eita pessoa boa!' });
 module.exports = createStudent;
