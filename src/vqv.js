7f535cddcae4713c0493e79043684739e3494777
Caio César <caiovg32@gmail.com>
2021-05-24 17:07:04 -0300

    test

--- src/vqv.js
+++ src/vqv.js
@@ -21,9 +21,9 @@ const vqv = (nome, idade) => {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
