aee9b6de713f1bb07098fb7fcec4c0353416aaea
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-05-24 16:29:35 -0300

    envia requisito 3 para conferencia 5

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,10 @@ const vqv = (nome, idade) => {
   if (typeof nome !== 'string' || typeof idade !== 'number') {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!
+  return `'Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  #VQV!'`;
 };
 
 module.exports = vqv;
