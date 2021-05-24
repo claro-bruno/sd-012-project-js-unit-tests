6a4e1aaddcfe0f6d99f9a1475d77b19de54e6b3c
Eduardo Linhares <modsgtasagame@gmail.com>
2021-05-24 15:54:48 -0300

    Resolve requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === null || idade === null) return undefined;
+  let str = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+  return str;
+};
 
 module.exports = vqv;
