39a2a20a4e3632b0899e3f64c58db957686c28a2
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-24 17:53:19 -0300

    Realiza requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (!nome||!idade) {
+    return undefined;
+  }
+  const frase = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+  return frase;
+};
 
 module.exports = vqv;
