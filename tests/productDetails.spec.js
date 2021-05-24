2904e27ddebc68ccf252305c33dbe37325041ce8
Tulio <tuliotrindade99@gmail.com>
2021-05-24 18:23:54 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,3 +1,4 @@
+const { strict } = require('assert');
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
@@ -30,12 +31,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(typeof(productDetails(String, String)), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productDetails(String, String).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof(productDetails(String, String)[0]), 'object');
+    assert.deepStrictEqual(typeof(productDetails(String, String)[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails(String, String)[0], productDetails(String, String)[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual((Object.values(Object.values(Object.values(productDetails(String, String)[0]))[1])[0]).substr(-3), '123')
+    assert.deepStrictEqual((Object.values(Object.values(Object.values(productDetails(String, String)[1]))[1])[0]).substr(-3), '123')
   });
 });
