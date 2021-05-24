b5493d1950cc351a77c3157d7ccf2c159256b4f0
Tulio <tuliotrindade99@gmail.com>
2021-05-24 15:25:33 -0300

    requisito 1 correto

--- src/average.js
+++ src/average.js
@@ -28,20 +28,3 @@ const average = (array) => {
   return Math.round(resultado);
 };
 module.exports = average;
-const assert = require('assert');
-
-    assert.strictEqual(average([3, 4, 5]), 4);
-    assert.strictEqual(average([1, 2, 3, '4', 5]), undefined);
-    assert.strictEqual(average([0, 0, 0, 0, 0, 0, 0]), 0);
-    assert.strictEqual(average([1, 2, '3']), undefined);
-    assert.strictEqual(average([1, 2, 3]), 2);
-    assert.strictEqual(average([0, 0, 0, 0, 0, 0, 1]), 0);
-
-    assert.strictEqual(average([]), undefined);
-    assert.strictEqual(average([' ']), undefined);
-    assert.strictEqual(average(['um', 'dois', 'tres']), undefined);
-    assert.strictEqual(average([47, 63, 122]), 77);
-
-    assert.strictEqual(average([-11, 2, 5]), -1);
-
-    assert.strictEqual(average([-11, -5, 2]), -5);
