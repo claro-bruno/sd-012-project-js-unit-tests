0bab43aeef67fb5daf2f0c77ba3c5540f9416817
Josimar Souza <contact.josimarsouza@gmail.com>
2021-05-24 16:05:52 -0300

    Implementa a funçao vqv resolvendo o 3 requisito

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let phrase;
+  if (nome === undefined || idade === undefined) {
+    return phrase;
+  }
+  phrase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return phrase;
+};
 
 module.exports = vqv;
