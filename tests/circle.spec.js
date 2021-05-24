6ce2bc3b74ded0c644b8bfad734b670d9a69db68
Guilherme Cesconeto <cesconeto@gmail.com>
2021-05-24 16:11:56 -0300

    Fazendo a 4 denovo

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,6 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    
     assert.strictEqual(typeof circle(3), 'object');
     assert.strictEqual(Object.keys(circle(5)).length, 3);
     assert.strictEqual(circle(), undefined);
