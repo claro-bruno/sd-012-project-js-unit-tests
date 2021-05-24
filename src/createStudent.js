9ae4211789a2dab82b5b615d9f2993b55ddbfe6c
camila-mp <ca.malvp@gmail.com>
2021-05-24 19:00:22 -0300

    completa requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  return {
+    name: name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+};
 
 module.exports = createStudent;
