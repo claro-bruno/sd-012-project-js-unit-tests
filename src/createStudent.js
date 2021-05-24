c1521f861afa7bbbdbe7f94110c666c55da7c26e
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-05-24 18:40:24 -0300

    create student ok

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const obj = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return obj;
+};
 
 module.exports = createStudent;
