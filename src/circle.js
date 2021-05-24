a6526e070ab0a11f0be1e7dd55b5b2988be2fd57
Roberval Filho <rslfilho@gmail.com>
2021-05-24 15:54:54 -0300

    Requisito 04 finalizado

--- src/circle.js
+++ src/circle.js
@@ -23,7 +23,7 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: PI * radius * radius,
+    area: parseFloat((PI * radius * radius).toPrecision(4)),
     circumference: 2 * PI * radius,
   };
 };
