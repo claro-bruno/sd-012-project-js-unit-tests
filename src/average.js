b376a0d0be9b8532823b183db97bee3c32bebf9a
Eduardo Linhares <modsgtasagame@gmail.com>
2021-05-24 15:39:39 -0300

    Resolve ESlint

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (array) => {
   if (!array.length) return undefined;
   for (let i = 0; i < array.length; i += 1) {
-    if (typeof(array[i]) !== 'number') {
+    if (typeof (array[i]) !== 'number') {
       return undefined;
     }
   }
