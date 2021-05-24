7db5e23dae66971510f101d34e5d674df9801685
Samuel Constantino <samuelicapuidados@gmail.com>
2021-05-24 17:26:38 -0300

    requisito 4: area para raio 3

--- src/circle.js
+++ src/circle.js
@@ -27,6 +27,6 @@ const circle = (radius) => {
     circumference: 2 * PI * radius,
   };
 };
-console.log(circle(2))
+console.log(circle(3))
 
 module.exports = circle;
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -36,6 +36,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(circle(3).area, 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
