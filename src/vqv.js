32cf638bcb1a2b5e0545d98901e32b8fa8389688
Tulio <tuliotrindade99@gmail.com>
2021-05-24 15:57:17 -0300

    correçao requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,8 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => `Oi, meu nome é ${nome}!
-Tenho ${idade} anos,
-trabalho na Trybe e mando muito em programação!
-#VQV!`;
+const vqv = (nome, idade) => {
+  if (typeof (nome) === 'string' && typeof (idade) === 'number') {
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+  }
+  return undefined;
+};
 module.exports = vqv;
