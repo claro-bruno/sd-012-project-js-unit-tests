b612ca074ee9e9527b53608db8e447a2fea5d7a3
Willian Prado <willian_prado@hotmail.com>
2021-05-24 18:49:25 -0300

    Resolve o requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,36 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // Implemente sua função aqui
+const isNumeric = (arr) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof (arr[i]) !== 'number') {
+      return false;
+    }
+  }
+  return true;
+};
+
+const notEmpty = (arr) => arr.length > 0;
+
+const average = (arr) => {
+  if (isNumeric(arr) && notEmpty(arr)) {
+    let total = 0;
+
+    for (let i = 0; i < arr.length; i += 1) {
+      total += arr[i];
+    }
+    let result = (total / arr.length);
+    return Math.round(result);
+  } 
 };
 
 module.exports = average;
+
+//  let myArr = [2, 2];
+//  let myArr = [];
+//  let myArr = [1, 2, 3, '4', 5];
+//  let myArr = ['um', 'dois', 'tres'];
+//  let myArr = [47, 63, 122];
+//  let myArr = [-11, 2, 5]; 
+//  let myArr = [-11, -5, 2];
+//  console.log(average(myArr));
