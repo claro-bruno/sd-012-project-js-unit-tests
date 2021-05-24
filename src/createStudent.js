a5ad2c1a798400ba80494d58a69c9893c628b3d2
Wellington <junior.wel02@exemplo.br>
2021-05-24 17:33:07 -0300

    5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let resposta = { name: null, feedback: null };
+  resposta.name = nome;
+  resposta.feedback = () => 'Eita pessoa boa!';
+  return resposta;
+};
 
 module.exports = createStudent;
