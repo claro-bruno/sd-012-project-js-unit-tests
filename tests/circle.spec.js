6feec5d46cb344587ae3578d9f48c9f6693d26c2
lucasnahas <lucasahas@gmail.com>
2021-05-24 16:34:35 -0300

    ex4 circle tests

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,15 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
+    assert.strictEqual(typeof circle(1), 'object');
+    assert.strictEqual(Object.entries(circle(1)).length, 3);
+    assert.strictEqual(circle(), undefined);
+    assert.strictEqual(circle(2).circumference, 12.56);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
+
+
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
