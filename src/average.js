749f003e7bd3fc3a50e8a089fa0c8df3886822cd
pedro21medeiros <pedro21medeiros@gmail.com>
2021-05-24 14:58:09 -0300

    Tenta requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let sum = 0;
+  if (array.length == 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] != 'number') {
+      return undefined;
+    }
+    sum = sum + array[index];
+  }
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
