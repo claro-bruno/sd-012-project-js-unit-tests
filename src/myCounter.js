f61a800b66700be82556d69d7791b57c32e8a7ee
pedro21medeiros <pedro21medeiros@gmail.com>
2021-05-24 18:10:30 -0300

    tenta requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,11 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counterAdd = 2; counterAdd <= 3; counterAdd += 1) {
+      myArray.push(counterAdd);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
