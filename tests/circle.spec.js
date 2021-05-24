47ba400bd3a07434283884b371057cf56c107500
Josimar Souza <contact.josimarsouza@gmail.com>
2021-05-24 17:17:35 -0300

    Adiciona mais 1 teste para a funçao circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -37,5 +37,6 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.strictEqual(Object.values(circle(2))[2], 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
   });
 });
