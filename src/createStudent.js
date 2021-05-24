c6bb35d0aa6af6ce0783193f32b5bb86692ce561
Stefano <stefanooliveira1@gmail.com>
2021-05-24 18:52:55 -0300

    Requisito 5 - finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,41 +14,13 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const pessoaBoa = () => {
-   return 'Eita pessoa boa!'
-}
-
 const createStudent = (nome) => {
    let student = {
       name: nome
    }
-   student.feedback = pessoaBoa();
-
+   student.feedback = () => {
+      return 'Eita pessoa boa!';
+   }
    return student;
 };
-
-
-
-const goodPerson = 'Eita pessoa boa!';
-const assert = require('assert');
-
-const estudante = createStudent('Leandrão, o Lobo Solitário');
-    assert.strictEqual(typeof estudante, 'object');
-    assert.strictEqual(typeof estudante.feedback, 'function');
-    assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
-    assert.strictEqual(estudante.feedback(), goodPerson);
-
-    const estudante2 = createStudent('Nobre');
-    assert.strictEqual(typeof estudante2, 'object');
-    assert.strictEqual(typeof estudante2.feedback, 'function');
-    assert.strictEqual(estudante2.name, 'Nobre');
-    assert.strictEqual(estudante2.feedback(), goodPerson);
-
-    const estudante3 = createStudent('Inácio');
-    assert.strictEqual(typeof estudante3, 'object');
-    assert.strictEqual(typeof estudante3.feedback, 'function');
-    assert.strictEqual(estudante3.name, 'Inácio');
-    assert.strictEqual(estudante3.feedback(), goodPerson);
-
 module.exports = createStudent;
