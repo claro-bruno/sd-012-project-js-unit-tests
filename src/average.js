c80172cecdb03a57db11048154cbeb103b0b7bcc
pedro21medeiros <pedro21medeiros@gmail.com>
2021-05-24 14:59:52 -0300

    Corrige erros linter requisito 1

--- src/average.js
+++ src/average.js
@@ -14,14 +14,14 @@
 
 const average = (array) => {
   let sum = 0;
-  if (array.length == 0) {
+  if (array.length === 0) {
     return undefined;
   }
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[index] != 'number') {
+    if (typeof array[index] !== 'number') {
       return undefined;
     }
-    sum = sum + array[index];
+    sum += array[index];
   }
   return Math.round(sum / array.length);
 };
