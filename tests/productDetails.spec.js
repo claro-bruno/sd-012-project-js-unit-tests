2a840e10a63eadbbd15a6d1ce845473e1735a34e
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-24 17:13:07 -0300

    (6) Finaliza testes productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const result = productDetails('Alcool gel', 'Máscara');
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof (result), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(result.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (result[0]) === 'object' && typeof (result[1]) === 'object', true);
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(result[0], result[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(result[0].details.productId.slice(-3) == 123 && result[1].details.productId.slice(-3) == 123, true );
   });
 });
