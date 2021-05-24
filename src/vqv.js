6457388d0d2ef274e55c5bda5d14866f2026e36e
Çuiz Henrique <luishenriqueee2010@hotmail.com>
2021-05-24 18:45:37 -0300

    Adiciona ponto de exclamacao ao nome

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,7 @@
 
 const vqv = (nome, idade) => {
   if (nome && idade) {
-    return `Oi, meu nome é ${nome}
+    return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
