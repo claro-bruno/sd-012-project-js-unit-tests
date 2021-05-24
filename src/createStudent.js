3d77078e13e0ce121c15bcf847f9f677da1d23eb
Daniel <danielbped@gmail.com>
2021-05-24 16:00:16 -0300

    Resolve requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+// const { create } = require("eslint/lib/rules/*");
+
+const createStudent = (name) => {
+  const feedback = () => 'Eita pessoa boa!';
+  const estudante = {name: name, feedback: feedback()};
+  return estudante;
+};
+
 
 module.exports = createStudent;
