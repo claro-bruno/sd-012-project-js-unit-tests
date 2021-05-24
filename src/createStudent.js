3855d051f136b35623fed054407c0dc5daffec02
Samuel Constantino <samuelicapuidados@gmail.com>
2021-05-24 17:50:11 -0300

    implementa requisito 5: criar estudante

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nomeEstudante) => {
+  const estudante = {
+    name: nomeEstudante,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
