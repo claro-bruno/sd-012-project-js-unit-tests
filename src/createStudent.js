b93b6cdc22c1d6c47d4c79081f23eabbb24786b3
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-24 18:59:01 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (name) => {
+  let object = {
+    name: name,
+    feedback: () => 'Eita pessoa boa!'
+  };
+  return object;
+};
+console.log(createStudent('nada').feedback)
 module.exports = createStudent;
