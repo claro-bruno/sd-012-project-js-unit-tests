5997db929b52be22e4d2b814fba970d0d702210e
Daniel <danielbped@gmail.com>
2021-05-24 15:45:54 -0300

    Corrige requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if (!nome || !idade) {
-    return undefined;
-  } else {
-    return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
-  }
+  if (!nome || !idade) { return undefined; } 
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
 };
 
 module.exports = vqv;
