f8698a03034deeacf4c44f38454ee2cde0abfb11
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-24 16:50:36 -0300

    Requisito 5 Pronto

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const student = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
 
 module.exports = createStudent;
