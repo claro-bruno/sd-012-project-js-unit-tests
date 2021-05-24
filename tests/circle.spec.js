5348e5575eed9fda841eaf3bd530226289522c06
ladcs <hpladcs@gmail.com>
2021-05-24 17:17:35 -0300

    R4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const assert = require('assert');
+const { pid } = require('process');
 const circle = require('../src/circle');
 
 /*
@@ -29,10 +30,13 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle(7), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.strictEqual(circle(7).lenght, 3);
+    assert.strictEqual(Object.keys(circle(7)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.deepStrictEqual(circle(2), {radius: 2, area: 12.56, circumference: 12.56});
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
