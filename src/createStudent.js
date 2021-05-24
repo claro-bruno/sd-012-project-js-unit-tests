3b8caf1f828723e15eaa595aa436e8cf01507535
Patrick Dack <patrickdack89@gmail.com>
2021-05-24 17:33:25 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (inputName) => ({ name: inputName, feedback: () => 'Eita pessoa boa!' });
 
 module.exports = createStudent;
