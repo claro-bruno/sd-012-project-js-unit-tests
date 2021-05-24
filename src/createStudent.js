5b31bbf417942624cb48a44d0f4ac13ade95f8c1
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-24 17:45:20 -0300

    Resolvido Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const frase = () => 'Eita pessoa boa!';
+  const objetoFinal = {
+    name: nome,
+    feedback: frase,
+  };
+  return objetoFinal;
+};
 
 module.exports = createStudent;
