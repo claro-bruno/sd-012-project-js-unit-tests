9a6579a97327e8c8b86f5e8a20f043e52d07f38f
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-05-24 16:19:02 -0300

    Requisito 03

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof nome === 'string' && typeof idade === 'number') {
+    const paragrafo = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+    return paragrafo;
+  }
+  return undefined;
+};
 
 module.exports = vqv;
