f52da850e53323a183ce25a34d05fac4d325bdca
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-05-24 17:23:48 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,11 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) { 
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`
+};
 
 module.exports = vqv;
