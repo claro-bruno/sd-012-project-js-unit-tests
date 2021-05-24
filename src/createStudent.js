ab3626522cda9570fa28da9032944e2de21d2050
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-24 16:53:07 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (identification) => {
+  const estudante = {
+    name: identification,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
