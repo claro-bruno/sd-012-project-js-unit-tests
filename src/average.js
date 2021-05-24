e0cabfeff95d4d56f2f3f44aa335a81e689902a4
Patrick Dack <patrickdack89@gmail.com>
2021-05-24 15:44:03 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-//
+const average = (arr) => {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof (arr[index]) !== 'number') {
+      return undefined;
+    }
+    sum += arr[index];
+  }
+  return Math.round(sum / arr.length);
+};
+console.log(average([-11, -5, 2]));
 module.exports = average;
