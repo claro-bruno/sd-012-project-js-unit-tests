7cd40b094b10915e5eca0b6dada2442f371713a2
pedro21medeiros <pedro21medeiros@gmail.com>
2021-05-24 17:07:59 -0300

    tenta requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (name) => {
+  const student = {
+    name,
+    feedback: () => 'Eita pessoa boa!'
+  };
+  return student;
+};
+// console.log(createStudent('Pedro'));
 module.exports = createStudent;
