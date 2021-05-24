ae782b701faeeb2c7bdf5e64bb306f2ddf234ed4
José Carlos <kalarruda@hotmail.com>
2021-05-24 15:18:38 -0300

    3-desafio conserta indentaçao novamente - vqv

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const goodPerson = 'Eita pessoa boa!';
+const createStudent = (name) => {
+  return {
+    name,
+    feedback: () => goodPerson,
+  };
+};
 module.exports = createStudent;
--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,7 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-    return `      Oi, meu nome é ${nome}!
+    return `       Oi, meu nome é ${nome}!
       Tenho ${idade} anos,
       trabalho na Trybe e mando muito em
       programação!
