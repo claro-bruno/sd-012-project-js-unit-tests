56d9c7f568ca7e3321dace2cb8dbb7f5987556b2
Eder Santos <3derms@gmail.com>
2021-05-24 18:38:17 -0300

    refatora a funçao e reorganiza os ifs e for

--- src/average.js
+++ src/average.js
@@ -14,13 +14,17 @@
 
 const average = (array) => {
   let valor = 0;
-  for (let index = 0; index < array.length; index += 1) {
-    valor = valor + array[index] / array.length;
-    if (typeof array[index] === 'string' || array.length === 0) {
-      return 'undefined';
+  if (array.length !== 0) {
+    for (let index = 0; index < array.length; index += 1) {
+      if (typeof (array[index]) == 'string') {
+        return undefined;
+      } else {
+          valor = valor + array[index] / array.length;
+        }
     }
-  } if ((valor % 2) % 2 !== 0 && (valor % 3) % 3 !== 0 && (valor % 5) % 5) {
-      valor = valor + 1 - (valor % 2);
+    return valor = Math.round(valor);
+  } else { 
+      return undefined;
     }
   return valor;
 };
