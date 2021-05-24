218e0b8f0e15f36c050b22efb4f7bd1286a47fa6
Thiago Prado <thiagopc_dev@hotmail.com>
2021-05-24 20:01:29 -0300

    Detalhe no Desafio 7

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
+  let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
