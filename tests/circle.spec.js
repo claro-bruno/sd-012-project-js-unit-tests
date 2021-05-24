970694650153bf564ce6c9aa84a25160d9b2eb41
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-24 16:12:06 -0300

    Requisito 4 | circle.spec.js | Corrige quarta assercao

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -33,7 +33,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.strictEqual(parseFloat((circle(3).circumference).toPrecision(4)), 12.56);
+    assert.strictEqual(parseFloat((circle(2).circumference).toPrecision(4)), 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.strictEqual(parseFloat((circle(3).area).toPrecision(4)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
