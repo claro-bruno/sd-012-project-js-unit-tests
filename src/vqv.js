fadabe5e6901d91f260420ee702d9d0c03bc0991
Tulio <tuliotrindade99@gmail.com>
2021-05-24 16:01:47 -0300

    correçao requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,7 @@
 
 const vqv = (nome, idade) => {
   if (typeof (nome) === 'string' && typeof (idade) === 'number') {
-  return `Oi, meu nome é ${nome}!
+  return `  Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
