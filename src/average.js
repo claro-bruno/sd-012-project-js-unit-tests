f5124870ad2693f00418ff94094ecff27381e404
Guilherme Cesconeto <cesconeto@gmail.com>
2021-05-24 15:44:58 -0300

    Fazendo a 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  if (array.length === 0) return undefined;
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') return undefined;
+    sum += array[index];
+  }
+  sum = Math.round(sum / array.length);
+  return sum;
 };
 
 module.exports = average;
