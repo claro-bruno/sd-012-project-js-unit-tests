a9de08dd8e012b6f5a901ef19411bd00be9da082
Roberval Filho <rslfilho@gmail.com>
2021-05-24 15:07:48 -0300

    Requisito 01 resolvido

--- src/average.js
+++ src/average.js
@@ -12,8 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+const average = (array) => {
+  if (array.length === 0) return undefined;
   
+  let sum = 0;
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') return undefined;
+    sum += array[index];
+  }
+
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
