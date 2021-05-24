36f3d7741347fd69759dbcc3f6cf17c50b1c0909
paulomichael <paulo.michael@gmail.com>
2021-05-24 18:15:30 -0300

    implementa a função createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nameString) => {
+  const estudante = { 
+    name: nameString,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+ };
 
 module.exports = createStudent;
