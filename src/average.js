27b43dbfedfdf352f80ae06d5fc0b979eb94c8cf
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-24 15:46:27 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (arr) => {
+  let sum = 0;
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof arr[index] !== 'number') {
+      return undefined;
+    }
+    sum += arr[index];
+  }
+  const roundedAverage = Math.round(sum / arr.length);
+  return roundedAverage;
 };
+
+console.log(average([1, 2, 4]));
 
 module.exports = average;
