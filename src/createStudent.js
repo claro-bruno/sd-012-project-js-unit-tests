3402bc3b7d5e55954be5184fcc9eb750df3c8180
RyanLaiber <ryanlaiber@gmail.com>
2021-05-24 18:11:33 -0300

    adiciona requisito 5, function createStudent.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const estudantes = { name: nome, feedback() { return 'Eita pessoa boa!'; } };
+  return estudantes;
+};
 
 module.exports = createStudent;
