715d857c5c53a3952b8f7e7a81d90b6d35fd4a9d
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-05-24 14:36:43 -0400

    resolve a função average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let soma = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'string') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+  return Math.round(soma / array.length);
+};
 
 module.exports = average;
