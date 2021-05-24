b1d633ded25059f6b4ab5c9fc49cf797c78e81b9
Josimar Souza <contact.josimarsouza@gmail.com>
2021-05-24 17:38:56 -0300

    Adiciona espaços no objecto da funçao createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => ({name: nome, feedback: () => 'Eita pessoa boa!'});
+const createStudent = (nome) => ({ name: nome, feedback: () => 'Eita pessoa boa!' });
 
 module.exports = createStudent;
