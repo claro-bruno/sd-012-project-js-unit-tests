9c1069f292e0b85cb8df0b9c14d72bb2ca53fbb5
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-24 17:17:13 -0300

    Requisito 3 correçao do lint

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,10 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === undefined || idade === undefined) {
+  if (nome === undefined && idade === undefined) {
     return undefined;
   }
-  return`Oi, meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
