71d796f8d6b4a3228a61da5f402ace97f7ab09ff
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-24 14:56:44 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/average.js
+++ src/average.js
@@ -12,6 +12,8 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = () => {
+  console.log('alteração para primeiro commit');
+};
 
 module.exports = average;
