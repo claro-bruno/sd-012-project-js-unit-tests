a86bdc520df3193a114f9de961c34f057049fdb0
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-05-24 15:21:13 -0300

    Satisfaz requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,34 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // VAMOS COM TUDO :rocket:
+const verifyArray = (arrayNum) => {
+  if (arrayNum.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < arrayNum.length; index += 1) {
+    if (typeof (arrayNum[index]) !== 'number') {
+      return undefined;
+    }
+  }
+
+  return 'a Array é valida';
 };
+
+const average = (arrayNum) => {
+  if (verifyArray(arrayNum) === undefined) {
+    return undefined;
+  }
+
+  let sum = 0;
+  for (let index = 0; index < arrayNum.length; index += 1) {
+    sum += arrayNum[index];
+  }
+
+  let averageArray = sum / arrayNum.length;
+  return Math.round(averageArray);
+};
+
+// console.log(average([3, 4, 5]));
 
 module.exports = average;
