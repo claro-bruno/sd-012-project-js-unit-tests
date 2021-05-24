d6586a9a160010d9e76a957df2453078e1287ac3
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-24 15:35:28 -0300

    adicionando requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  return {
+    name: nome,
+    feedback: () => {
+      return 'Eita pessoa boa!'
+    },
+  };
+};
 
 module.exports = createStudent;
