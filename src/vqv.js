6f252f6b00ec17f32499f928b90bb68205884da0
Eurides <euridesalves@yahoo.com.br>
2021-05-24 17:52:41 -0300

    Corrigido requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,11 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+};
 
 module.exports = vqv;
