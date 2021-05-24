c3a6dcdfbe69f2bf4f2bde47a61ca71696e50968
cristianocsl <cslcristiano@gmail.com>
2021-05-24 16:16:41 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/average.js
+++ src/average.js
@@ -12,6 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number' || typeof array[index] === 'string') {
+      return undefined;
+    } else {
+      sum += array[index];
+    }
+  }
+  let quantNum = array.length;
+  const average = Math.round(sum / quantNum);
+  
+  return average;
+};
 
 module.exports = average;
