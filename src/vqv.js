e7f087736e8b938321da6ec457ecc2ec3e2817bd
MaxAlves0035 <maxalves2@hotmail.com>
2021-05-24 19:11:40 -0300

    requisito 3 - correcao

--- src/vqv.js
+++ src/vqv.js
@@ -20,12 +20,11 @@ const vqv = (nome, idade) => {
 if (typeof nome !== 'string' || typeof idade !== 'number') {
   return undefined;
 }
-let texto = `'Oi, meu nome é ${nome}!
+let texto = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!'`;
+#VQV!`;
 return texto;
 };
-console.log(vqv());
 
 module.exports = vqv;
