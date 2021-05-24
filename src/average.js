526b36622f505312c4133affc7f729b25d70bb62
Ramon Paschoal <rpaschoal90@gmail.com>
2021-05-24 16:14:51 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+// const assert = require('assert');
+
+const average = (array) => {
+  let soma = 0;
+  let media = 0;
+  if (array.length < 1) {
+    return undefined
+  }
+  for(let index = 0; index < array.length; index += 1) {
+    if(typeof array[index] !== 'number'){
+      return undefined;
+    }
+  soma += array[index];
+  }
+  media = soma/array.length;
+  return Math.round(media);
+};
 
 module.exports = average;
