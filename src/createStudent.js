c87564f4b37a54ea7fbf2cf446b1850771d121f8
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-05-24 19:11:11 -0300

    Resolve o requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => ({ name: nome, feedback: () => 'Eita pessoa boa!' });
 
 module.exports = createStudent;
