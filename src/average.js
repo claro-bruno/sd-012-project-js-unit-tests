5299b2edda35bd03075612f1de199987fab033d8
alestori <alestori7@gmail.com>
2021-05-24 14:40:26 -0300

    Primeiro commit: completa requisito 1; npm install

--- src/average.js
+++ src/average.js
@@ -12,6 +12,11 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = () => {
+  if (array.length === 0) return undefined;
+  const sum = array.reduce((a, b) => a + b);
+  if (typeof sum !== 'number') return undefined;
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
