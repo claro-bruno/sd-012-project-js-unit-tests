fc7db7b7eaeb9a7d29ff28e2718ea573e3add75c
Eric Faria <ericdeofaria@gmail.com>
2021-05-24 15:00:55 -0300

    Requisito 01

--- src/average.js
+++ src/average.js
@@ -1,7 +1,8 @@
 const average = (array) => {
   let sum = 0;
+  if (array.length === 0) return undefined;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof (array[index]) !== 'number') return undefined;
+    if ((typeof (array[index]) !== 'number')) return undefined;
     sum += array[index];
     }
   return sum / array.length;
