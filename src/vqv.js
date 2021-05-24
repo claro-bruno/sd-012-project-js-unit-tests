bd4bacf98644dae0c0027aba43f64c045da4d48b
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-05-24 15:40:54 -0300

    Requisito 3 completo

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,22 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined && idade === undefined) {
+    return undefined;
+  }
+  const string = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+
+  return string;
+};
+
+console.log(vqv('Tunico', 30))
+console.log('Oi, meu nome é Alberto!\n' +
+  'Tenho 30 anos,\n' +
+  'trabalho na Trybe e mando muito em programação!\n' +
+  '#VQV!')
 
 module.exports = vqv;
