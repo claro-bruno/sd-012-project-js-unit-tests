dec9445a7e5033812a243753f40c3df282962fbe
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-24 16:07:15 -0300

    Requisito 4 | circle.spec.js | Corrige propriedade length

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,7 +29,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.strictEqual(Object.entries.length(circle(1)), 3);
+    assert.strictEqual(Object.entries(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
