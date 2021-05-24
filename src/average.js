15da96b28f16bebabe12452f3a3c6e578beabee6
Eduardo Linhares <modsgtasagame@gmail.com>
2021-05-24 15:38:49 -0300

    Resolve requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (!array.length) return undefined;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof(array[i]) !== 'number') {
+      return undefined;
+    }
+  }
+  return Math.ceil(array.reduce((ac, valor) => ac + valor) / array.length);
+};
 
 module.exports = average;
