bfca5c1674a0009411d5a56b9f3cb5b2ce6d9fcc
Stefano <stefanooliveira1@gmail.com>
2021-05-24 19:19:04 -0300

    corrige req 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -26,12 +26,12 @@ const circle = require('../src/circle');
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    assert.strictEqual(typeof (circle(1)), 'object');
+    assert.strictEqual(typeof circle(1), 'object');
     assert.strictEqual(Object.entries(circle(1)).length, 3);
     assert.strictEqual(circle(), undefined);
-    assert.strictEqual(circle(2).circumference, 12.56)
-    assert.strictEqual(circle(3).area, 28.259999999999998)
-    assert.strictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
+    assert.strictEqual(circle(2).circumference, 12.56);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
 
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
