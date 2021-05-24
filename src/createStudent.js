71ba2b3e84f7e2dbd0916175f7d105ebf9dac837
ladcs <hpladcs@gmail.com>
2021-05-24 18:00:20 -0300

    R5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const objectValor = {
+    name: nome, 
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return objectValor;
+};
 
 module.exports = createStudent;
