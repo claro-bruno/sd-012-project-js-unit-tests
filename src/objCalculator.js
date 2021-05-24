2bce9563b7e2e1223194b355eca8e2347a877858
Pedro Gonçalves <peg.gep27@gmail.com>
2021-05-24 18:13:50 -0300

    Correçoes requisito 6

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -23,6 +23,6 @@ const calculator = ({
   add: (n1, n2) => n1 + n2,
   mult: (n1, n2) => n1 * n2,
   div: (n1, n2) => Math.trunc(n1 / n2),
-  sub: (n1, n2) => n1 * n2,
+  sub: (n1, n2) => n1 - n2,
 });
 module.exports = calculator;
