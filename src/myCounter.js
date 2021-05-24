b487710b352c2672c0f8131ad4ce1a7a8fcf9338
Teika <shimworks.tk@gmail.com>
2021-05-24 18:24:10 -0300

    corrige requisito 08

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,7 +20,7 @@ const myCounter = () => {
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
-      myArray.push(counter);
+      myArray.push(counter1);
     }
   }
   return myArray;
