9e07d2861dccb6b83e7e58c474b7c0dead57aaad
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-24 18:08:21 -0300

    Corrige Lint

--- src/vqv.js
+++ src/vqv.js
@@ -20,8 +20,7 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  let frase = 
-`Oi, meu nome é ${nome}!
+  let frase = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
