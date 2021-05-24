335a9ea6bf497dfc68265bce49a9e6772b5c4ad1
José Carlos <kalarruda@hotmail.com>
2021-05-24 15:37:24 -0300

    3-desafio indentacao dificil de acertar

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,10 +30,12 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(typeof productDetails(), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara'), [ { name: 'Alcool gel', details: { productId: 'Alcool gel123' } },
+    { name: 'Máscara', details: { productId: 'Máscara123' } } ])
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
