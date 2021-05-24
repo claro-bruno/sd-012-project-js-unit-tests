5a905317a1972f3dee617ba1ef924a3f11c9db63
Çuiz Henrique <luishenriqueee2010@hotmail.com>
2021-05-24 17:49:03 -0300

    Testa requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,10 +18,16 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    assert.fail();
+    const assert = require('assert');
+    const numbers = require('../src/numbers');
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true, 'não tem apenas números')
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false, 'não tem apenas números')
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.deepStrictEqual(numbers([1,'a', 3]), false, 'não tem apenas números')
     // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.deepStrictEqual(numbers([' ']), false, 'não tem apenas números')
+
   });
 });
