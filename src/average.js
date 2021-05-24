a9e5f8d23ce025f12767a454d43c6df6ea2afaf4
Janderson Soares <jandinsm@gmail.com>
2021-05-24 17:47:39 -0300

    Desafio '

--- src/average.js
+++ src/average.js
@@ -12,7 +12,41 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let result = 0;
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'number') {
+      soma += array[index];
+    } else {
+      return undefined;
+    }
+  }
+    result = Math.round((soma / array.length));
+    return result;
+};
+// Socorro
+let teste = [1, 2, '3'];
+console.log(average(teste));
+// const assert = require('assert');
 
+// assert.strictEqual(average([3, 4, 5]), 4);
+//     assert.strictEqual(average([1, 2, 3, '4', 5]), undefined);
+//     assert.strictEqual(average([0, 0, 0, 0, 0, 0, 0]), 0);
+//     assert.strictEqual(average([1, 2, '3']), undefined);
+//     assert.strictEqual(average([1, 2, 3]), 2);
+//     assert.strictEqual(average([0, 0, 0, 0, 0, 0, 1]), 0);
+
+//     assert.strictEqual(average([]), undefined);
+//     assert.strictEqual(average([' ']), undefined);
+//     assert.strictEqual(average(['um', 'dois', 'tres']), undefined);
+//     assert.strictEqual(average([47, 63, 122]), 77);
+
+//     assert.strictEqual(average([-11, 2, 5]), -1);
+
+//     assert.strictEqual(average([-11, -5, 2]), -5);
 
 module.exports = average;
