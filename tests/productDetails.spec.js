483696b842339904c302adb48875b47b695a6639
GMODias <gmd.delta@gmail.com>
2021-05-24 20:37:46 -0300

    [PT-BR] Atendendo ao requisito 6. [EN] Solved 6th requirement.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,16 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(typeof(productDetails('Alcool gel', 'Máscara')), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(Object.keys(productDetails('Alcool gel', 'Máscara'),'object').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(((Object.values(productDetails('Alcool gel', 'Máscara'))).some((ele) => (typeof (ele) !== 'object'))), false);
     // Teste que os dois objetos são diferentes entre si.
+    assert.deepStrictEqual(JSON.stringify(productDetails('Alcool gel', 'Máscara'))[0]==JSON.stringify(productDetails('Alcool gel', 'Máscara'))[1], false);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(((productDetails('Alcool gel', 'Máscara')[0].details.productId).slice(-3) === '123') && ((productDetails('Alcool gel', 'Máscara')[1].details.productId).slice(-3) === '123'), true);
   });
 });
