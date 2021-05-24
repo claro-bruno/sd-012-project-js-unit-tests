490c55d10f03d8038b7768176587fd18448afeae
camila-mp <ca.malvp@gmail.com>
2021-05-24 18:05:32 -0300

    completa requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,12 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (!nome && !idade) {
+    return undefined;
+  }
+  const frase = 'Oi, meu nome é ' + nome + '!\n' + 'Tenho ' + idade + ' anos,\n' + 'trabalho na Trybe e mando muito em programação!\n' + '#VQV!';
+  return frase;
+};
 
 module.exports = vqv;
