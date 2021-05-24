c488a1dd6e7089e10adbb813033435e2de3dbe51
Gabriel Takaki <gabitj@icloud.com>
2021-05-24 17:59:13 -0300

    Requisito 7 (2)

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,10 +20,15 @@
 */
 
 const calculator = {
-  sum: (num1, num2) => num1 + num2,
+  add: (num1, num2) => num1 + num2,
   mult: (num1, num2) => num1 * num2,
   div: (num1, num2) => Math.floor(num1 / num2),
   sub: (num1, num2) => num1 - num2,
 };
+
+console.log(calculator.mult(10, 3));
+console.log(calculator.div(5, 2));
+console.log(calculator.sub(1, 3));
+console.log(calculator.add(1, 3));
 
 module.exports = calculator;
