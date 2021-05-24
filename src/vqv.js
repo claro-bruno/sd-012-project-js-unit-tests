c3a5179092d4ef0f189033fc5dd4ab25b7dcda60
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-05-24 17:40:38 -0300

    Resolvendo problema de undefined no requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,10 +16,12 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => 
-  (`Oi, meu nome é ${nome}!
+const vqv = (nome, idade) => { 
+  if (nome !== String || idade !== Number) { return undefined; }
+  return (`Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`);
+#VQV!`); 
+};
 
 module.exports = vqv;
