eb28194150f5a7e853ca14b5f2ac00b5c0f9573e
Eric Faria <ericdeofaria@gmail.com>
2021-05-24 14:46:05 -0300

    Requisito 01

--- src/average.js
+++ src/average.js
@@ -10,9 +10,16 @@
     - average([2, 2]) // Retorno: 2;
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
-    
+
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') return undefined;
+    sum += array[index];
+    }
+  return sum / array.length;
+};
 
 module.exports = average;
