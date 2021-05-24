ba92077045228072c7672a276ccb76e8d4ca0e7a
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-24 16:08:45 -0300

    (1) Finaliza funcao average

--- src/average.js
+++ src/average.js
@@ -12,8 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  if (array.length === 0 || typeof (array) !== 'object') {
+    return undefined;
+  }
+  let sum = 0;
+  for (const number of array) {
+    if (typeof number !== 'number') {
+      return undefined;
+    }
+    sum += number;
+  }
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
