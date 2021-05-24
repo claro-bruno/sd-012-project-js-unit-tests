35afe13ddbcf3e83edf8ac9fcc3f8b1e6c60c59f
Teika <shimworks.tk@gmail.com>
2021-05-24 18:12:47 -0300

    resolve lint requisito 08

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,7 +19,7 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
       myArray.push(counter);
     }
   }
