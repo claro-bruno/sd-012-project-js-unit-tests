484f62117b896076871c4fd6d58bc36546e048a1
Pedro Gonçalves <peg.gep27@gmail.com>
2021-05-24 15:41:31 -0300

    ES lint requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -18,10 +18,10 @@
 
 const vqv = (nome, idade) => {
   if (typeof nome === 'undefined' || typeof idade === 'undefined') return undefined;
-  return `'Oi, meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!'`;
+#VQV!`;
 };
 
 module.exports = vqv;
