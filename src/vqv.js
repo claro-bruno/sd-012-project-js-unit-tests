2357f769225cad65cb6fdd371181797d70a211e6
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-24 16:25:52 -0300

    corrige requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,33 +20,10 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
-
-const assert = require('assert');
-assert.strictEqual(typeof vqv, 'function');
-assert.strictEqual(typeof vqv('Tunico', 30), 'string');
-assert.strictEqual(
-  vqv('Tunico', 29),
-  'Oi, meu nome é Tunico!\n' +
-  'Tenho 29 anos,\n' +
-  'trabalho na Trybe e mando muito em programação!\n' +
-  '#VQV!',
-);
-assert.strictEqual(
-  vqv('Alberto', 30),
-  'Oi, meu nome é Alberto!\n' +
-  'Tenho 30 anos,\n' +
-  'trabalho na Trybe e mando muito em programação!\n' +
-  '#VQV!',
-);
-assert.strictEqual(
-  vqv('Hamaji', 29),
-  'Oi, meu nome é Hamaji!\n' +
-  'Tenho 29 anos,\n' +
-  'trabalho na Trybe e mando muito em programação!\n' +
-  '#VQV!',
-);
-assert.strictEqual(vqv(), undefined);
 
 module.exports = vqv;
