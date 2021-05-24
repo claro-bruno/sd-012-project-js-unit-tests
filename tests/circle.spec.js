4706085c672d9786657422bdd35b313362c028ee
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-05-24 17:37:54 -0300

    Adiciona refatoraçao no requisito 4 e adiciona requisito 5

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,9 +27,10 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.deepStrictEqual(typeof(circle(1)), 'object');
+    const circle1 = circle(1);
+    assert.deepStrictEqual(typeof(circle1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.deepStrictEqual(circle(1), {radius: 1, area: 3.14, circumference: 6.28});
+    assert.deepStrictEqual(circle(1), {radius: 1, area: 3.14, circumference: 6.28})
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.deepStrictEqual(circle(), undefined)
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
