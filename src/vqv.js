5b0052bccec52cbed29fbe484f02ec81af08ccd4
julio11lp <julio11lp@hotmail.com>
2021-05-24 16:45:56 -0300

    Criterio 3 aprovado no lint e test

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+
+  return `Oi, meu nome é ${nome}!\n`
++ `Tenho ${idade} anos,\n`
++ 'trabalho na Trybe e mando muito em programação!\n'
++ '#VQV!';
+};
 
 module.exports = vqv;
