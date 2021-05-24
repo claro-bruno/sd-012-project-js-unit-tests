17b5af8e3f4c390bc96af807cb52aba8f8a66bd7
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-24 18:40:02 -0300

    requisito 4

---
+++ tests/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+describe('4 - Implemente os casos de teste para a função `circle`', () => {
+  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(), 'object'); 
+    // Teste se o objeto retornado tem 3 entradas.
+    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+  });
+});
