7b813d4974913ba79a189c9fecdafda531821bb4
jmlinger <jmlinger10@gmail.com>
2021-05-24 20:24:44 -0300

    resolve requisito 4

--- src/vqv.js
+++ src/vqv.js
@@ -17,9 +17,7 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome == null && idade == null) {
-    return undefined;
-  }
+  if (!nome || !idade) return undefined;
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
