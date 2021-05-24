95e10948439fc4ff0eb61f897f63ebe5a10f2af0
Pedro Gonçalves <peg.gep27@gmail.com>
2021-05-24 16:43:48 -0300

    ESlint requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
-  return {
+const createStudent = (name) => ({
     name,
-    //feedback: () => ('Eita pessoa boa!'),
-  };
-};
+    feedback: () => 'Eita pessoa boa!',
+});
 
 module.exports = createStudent;
