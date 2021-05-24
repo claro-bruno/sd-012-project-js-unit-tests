8d3c2d7287f146dcab16a3c6c34d4c2b1595be05
André Moreno <deco616@hotmail.com>
2021-05-24 19:50:23 -0300

    Ajustando requisito 3, templetes literal nao deve ser espacado

--- src/vqv.js
+++ src/vqv.js
@@ -17,8 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof(nome) === 'string' && typeof(idade) === 'number'){
-    return 'Oi, meu nome é ' + nome + '!\n' +  'Tenho ' + idade +' anos,\n' +  'trabalho na Trybe e mando muito em programação!\n' +  '#VQV!';
+  if (typeof nome === 'string' && typeof idade === 'number') {
+    return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   }
 };
 
