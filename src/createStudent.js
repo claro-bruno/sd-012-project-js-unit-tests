b11fb07a19d2811383a84fe0bffb9d21a367d5e9
Thiago Prado <thiagopc_dev@hotmail.com>
2021-05-24 18:38:29 -0300

    Desafio 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const feed = () => 'Eita pessoa boa!'; 
+  return {
+    name: nome,
+    feedback: feed,
+  };
+};
 
 module.exports = createStudent;
