df6e32680684f6543e896746ae1194cca96d81d4
tiagoornelas <advtiagoornelas@gmail.com>
2021-05-24 17:36:46 -0300

    Resolve requisito 3

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let obj = {};
+  obj.name = name;
+  obj.feedback = () => `Eita pessoa boa!`;
+  return obj;
+};
 
 module.exports = createStudent;
