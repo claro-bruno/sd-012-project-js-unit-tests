a06c412b2b02e7c687a4a8ae73d23f0f16041a16
MGSiqueira <marcos.siqueira98@gmail.com>
2021-05-24 19:31:21 -0300

    Requisito 8 pronto

--- src/circle.js
+++ src/circle.js
@@ -27,5 +27,4 @@ const circle = (radius) => {
     circumference: 2 * PI * radius,
   };
 };
-console.log(Object.values(circle(3)));
 module.exports = circle;
--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,13 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let cont = 2; cont <= 3; cont += 1) {
+      myArray.push(cont);
     }
   }
   return myArray;
 };
-
 module.exports = myCounter;
