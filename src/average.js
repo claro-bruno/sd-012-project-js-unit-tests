5b3203db0cf964f0c331087c284afe9a52efbd20
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-24 17:13:21 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -11,9 +11,18 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
-const average = () => {
-  // add your implemetation here
+const average = (array) => {
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    soma += (array[index]);
+  }
+  return (Math.round(soma / (array.length)));
 };
 
 module.exports = average;
