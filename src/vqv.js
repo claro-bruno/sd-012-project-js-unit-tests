a0b3199579c87c4a36c3508423490837af25c278
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-05-24 18:49:42 -0300

    corrige function do desafio_03

--- src/vqv.js
+++ src/vqv.js
@@ -16,10 +16,10 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => `'Oi, meu nome é ${nome}!
+const vqv = (nome, idade) => `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
-  #VQV!'`;
+  #VQV!`;
 
 module.exports = vqv;
 
