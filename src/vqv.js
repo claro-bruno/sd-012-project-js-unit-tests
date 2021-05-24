80d49f23a4ce857fdb46425e9c685bea4e97509a
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-24 18:06:46 -0300

    Corrige requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,11 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  const frase = `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  let frase = 
+`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   return frase;
 };
 
