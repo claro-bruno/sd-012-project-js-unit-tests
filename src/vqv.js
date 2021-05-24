23afea8c203b21e1fae08821ea2ee8a13e748f8a
lucasnahas <lucasahas@gmail.com>
2021-05-24 16:05:33 -0300

    ex3 vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  const str = `'Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!'`;
+  return str;
+};
 
 module.exports = vqv;
+console.log(vqv('Tunico'));
