16e3cbad40e032aca454d3a9a7941b0252e03b47
alestori <alestori7@gmail.com>
2021-05-24 16:13:28 -0300

    requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (x, y) => x + y,
+  mult: (x, y) => x * y,
+  div: (x, y) => Math.floor(x / y),
+  sub: (x, y) => x - y,
+};
 
 module.exports = calculator;
