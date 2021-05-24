0f480ecf46ae4172f9d79299279668b3cd38b0dd
Caio César <caiovg32@gmail.com>
2021-05-24 17:33:29 -0300

    ajuste lint

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,13 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  return {
-    name: nome,
-    feedback: () => {
-      return 'Eita pessoa boa!';
-    },
-  };
-};
+const createStudent = (nome) => ({
+  name: nome,
+  feedback: () => 'Eita pessoa boa',
+});
 
 module.exports = createStudent;
