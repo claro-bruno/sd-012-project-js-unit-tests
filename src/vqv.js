705b9f9c55a95b7e8bc77f882135f84446a442ce
RyanLaiber <ryanlaiber@gmail.com>
2021-05-24 17:01:27 -0300

    adiciona requisito 3, function vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof (nome) && typeof (idade) === 'undefined') {
+    return undefined;
+  }
+
+  return (`Oi, meu nome é ${nome}!\n`
+  + `Tenho ${idade} anos,\n`
+  + 'trabalho na Trybe e mando muito em programação!\n#VQV!'); 
+};
 
 module.exports = vqv;
