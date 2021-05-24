dc81256abcd7c65756c3fd00d2e399498fc11d41
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-05-24 18:11:48 -0300

    Adiciona requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const verifyDetails = productDetails('Máscara','Sabonete');
+    assert.deepStrictEqual(typeof(verifyDetails), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(verifyDetails.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof(verifyDetails[0]), 'object');
+    assert.deepStrictEqual(typeof(verifyDetails[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.deepStrictEqual(verifyDetails[1] !== verifyDetails[0], true);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const pro1length = verifyDetails[0].details.productId.length;
+    const pro2length = verifyDetails[1].details.productId.length;
+    const product1ID = `${verifyDetails[0].details.productId[pro1length - 3]}${verifyDetails[0].details.productId[pro1length - 2]}${verifyDetails[0].details.productId[pro1length - 1]}`;
+    const product2ID = `${verifyDetails[1].details.productId[pro2length - 3]}${verifyDetails[1].details.productId[pro2length - 2]}${verifyDetails[1].details.productId[pro2length - 1]}`;
+    assert.deepStrictEqual(product1ID === '123' && product2ID === '123', true);
+
   });
 });
