27a42e6c516721ac2bd679d8dc958576e1842ddc
Willian Prado <willian_prado@hotmail.com>
2021-05-25 11:39:24 -0300

    Resolve o requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const object = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return object;
+};
 
 module.exports = createStudent;
