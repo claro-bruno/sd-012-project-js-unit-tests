17f9a27024bf7b06c1ed70a52794aa3fdfe185d9
Armando <a.guerra58@hotmail.com>
2021-05-24 22:19:28 -0300

    requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,28 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+
+  add: (number1, number2) => {
+    let sum = Math.floor(number1 + number2);
+    return sum;
+  },
+
+  mult: (number1, number2) => {
+    let multiplication = Math.floor(number1 * number2);
+    return multiplication;
+  },
+
+  div: (number1, number2) => {
+    let division = Math.floor(number1 / number2);
+    return division;
+  },
+
+  sub: (number1, number2) => {
+    let subtract = Math.floor(number1 - number2);
+    return subtract;
+  },
+
+};
 
 module.exports = calculator;
