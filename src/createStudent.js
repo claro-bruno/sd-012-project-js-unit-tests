a7d0c129dbe715026bc644b69f06e72a1dc74b07
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-24 22:53:24 -0300

    Requisito 5 - Concluido

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (inputName) => {
+  return {
+    name: inputName,
+    feedback: feedback = () => 'Eita pessoa boa!'
+  };
+};
 
 module.exports = createStudent;
