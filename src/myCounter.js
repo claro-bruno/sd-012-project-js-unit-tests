4e951854016d02514d6af8ff5f9c8c49511356ac
Stefano <stefanooliveira1@gmail.com>
2021-05-24 21:53:17 -0300

    corrige o linter chato

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let c = 2; c <= 3; c += 1) {
+      myArray.push(c);
     }
   }
   return myArray;
