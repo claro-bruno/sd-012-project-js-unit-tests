a91ee72f7df5c9b512179cc4e5da6a6110078cd2
Eder Santos <3derms@gmail.com>
2021-05-25 13:19:50 -0300

    corrige condiçao linha  20 e identaçao linha 26

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === '') {
+  if (typeof nome !== 'string' && typeof idade !== 'string') {
     return undefined;  
-  }
-  return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  } 
+return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
