992069926e5a378f16c5ee34e980af07c2748058
Augusto <augustocm2002@yahoo.com.br>
2021-05-24 22:29:53 -0300

    Corrigindo requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,10 @@
 */
 
 const vqv = (nome, idade) => {
-  const message = `Oi, meu nome é ${nome}!
+  const message = `'Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  #VQV!'`;
 
   return message;
 };
