0a3521faeca059726a697a73b4d144f10a5a5431
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-25 15:53:59 -0300

    Cria funçao para requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let estudante = {};
+  estudante.name = nome;
+  estudante.feedback = () => 'Eita pessoa boa!';
+  return estudante;
+};
 
 module.exports = createStudent;
