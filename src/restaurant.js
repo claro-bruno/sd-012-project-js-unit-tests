eda0815bd0cbe9f7791e8d7bef65401f158b1a05
Eric Kreis <erickreis793@gmail.com>
2021-05-24 22:15:37 -0300

    Refatorando

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,13 +1,11 @@
 /* eslint-disable max-len */
-function compareKeys(menu, menuKeys, menuItemsKeys, index) {
+const compareKeys = (menu, menuKeys, menuItemsKeys, index) => {
   let cost = 0;
   for (const item of menu.consumption) {
-    if (menuItemsKeys.includes(item)) {
-      cost += menu.fetchMenu()[menuKeys[index]][item];
-    }
+    if (menuItemsKeys.includes(item)) cost += menu.fetchMenu()[menuKeys[index]][item];
   }
   return cost;
-}
+};
 
 const createMenu = (objeto) => (
   {
@@ -20,8 +18,6 @@ const createMenu = (objeto) => (
       const menuKeys = Object.keys(this.fetchMenu());
       for (let index = 0; index < menuKeys.length; index += 1) {
         const menuItemsKeys = Object.keys(this.fetchMenu()[menuKeys[index]]);
-        console.log(menuItemsKeys);
-        console.log(menuKeys[index]);
         cost += compareKeys(menu, menuKeys, menuItemsKeys, index);
       }
       return Number((cost * 1.10).toFixed(2));
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,50 +1,5 @@
 const assert = require('assert');
 const createMenu = require('../src/restaurant');
- 
-/*
-  Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
-  - ler o menu cadastrado;
-  - fazer pedidos;
-  - verificar o que foi pedido;
-  - somar o valor da conta.
-
-  A estrutura deste código e deste objeto já foi definida e você irá implementá-la.
-  Abaixo você verá uma série de testes e passos que devem ser, NECESSARIAMENTE, feitos em ordem para o bom desenvolvimento do sistema. Eles guiarão você pelo desenvolvimento.
-
-  Parâmetros:
-  - Um objeto. Exemplos: { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }.
-  Comportamento:
-
-  const meuRestaurante = createMenu({ food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }).
-
-  meuRestaurante.fetchMenu() // Retorno: { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }
-
-  meuRestaurante.order('coxinha') // Retorno: undefined
-
-  meuRestaurante.consumption // Retorno: ['coxinha']
-
-  meuRestaurante.pay() // Retorno: 3.9
-
-  Uma função createMenu retorna um objeto com as seguintes características:
-  - Uma chave `fetchMenu` retorna o objeto que a função `createMenu` recebe por parâmetro. O menu tem sempre duas chaves, `food` e `drink`, no seguinte formato:
-
-  const meuRestaurante = createMenu({
-    food: {'coxinha': 3.90, 'sanduiche', 9.90},
-    drinks: {'agua': 3.90, 'cerveja': 6.90}
-  });
-
-  meuRestaurante.fetchMenu() // Retorno: Menu acima
-
-  - Uma chave `consumption` que contém um array de strings, com cada string sendo a chave de um pedido. Por exemplo: ['coxinha', 'cerveja']
-
-  - Uma chave `order` que tem uma função que, recebida uma string como parâmetro, adiciona essa string à lista salva em `consumption`.
-
-  - Uma chave `pay` que, quando chamada, invoca uma função que soma o valor de todos os pedidos e dá o preço com acréscimo de 10%.
-
-  IMPORTANTE: FAÇA OS TESTES E PASSOS DE ACORDO COM A ORDEM INDICADA!
-
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
 
 describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
@@ -91,10 +46,12 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // --------------------------------------------------------------------------------------
     // TESTE 8: Verifique que, ao chamar `objetoRetornado.pay()`, retorna-se a soma dos preços de tudo que foi pedido, conforme registrado em `objetoRetornado.consumption`
     // ```
-    const meuRestaurante = createMenu({
+    const meuRestaurante = createMenu(
+      {
       food: {'coxinha': 3.90, 'sanduiche': 9.90},
       drinks: {'agua': 3.90, 'cerveja': 6.90}
-    });
+      }
+    );
     meuRestaurante.order('coxinha');
     meuRestaurante.order('agua');
     meuRestaurante.order('coxinha');
