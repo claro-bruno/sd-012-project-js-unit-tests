4f307b0268b23432b80499c4e5fee89bb8b5848a
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-25 11:22:50 -0300

    corrige myCounter conforme lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let secondCounter = 2; secondCounter <= 3; secondCounter += 1) {
+      myArray.push(secondCounter);
     }
   }
   return myArray;
