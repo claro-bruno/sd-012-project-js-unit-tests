c7ede7f046be8005d4dbeefe5827cbe4e685f622
rachidb5 <66842598+rachidb5@users.noreply.github.com>
2021-05-25 20:18:53 -0300

    Adiciona função VQV

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,7 @@
 const vqv = (nome, idade) => {
   if (nome === null || idade === null) {
   return undefined;
-  } `Oi, meu nome é ${nome}!
+  } return `Oi, meu nome é ${nome}!
     Tenho ${idade} anos,
     trabalho na Trybe e mando muito em programação!
    #VQV!`; 
