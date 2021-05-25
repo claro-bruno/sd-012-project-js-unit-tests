26148faea461d6a89a28dbb29506063e66c64120
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-05-25 10:05:45 -0300

    Adiciona requisito 6.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { type } = require('os');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -30,12 +31,17 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('GTA', 'Resident Evil')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('GTA', 'Resident Evil').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(productDetails('Tomb Raider', 'Bloons TD 6').map((j) => typeof j), ['object', 'object']);
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Hellblade', 'Total War')[0], productDetails('Hellblade', 'Total War')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(productDetails('Lara Croft', 'Resident Evil - Code: Veronica').map((j) => parseFloat(j.details.productId.substr(j.details.productId.length - 3, j.details.productId.length))), [123, 123]);
   });
 });
