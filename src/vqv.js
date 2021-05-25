9f98410428abfaffe39de0400190b90266faccdc
Gabriel Viana <g.viana87@hotmail.com>
2021-05-25 00:44:17 -0300

    3 altera If p undefined

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,7 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof (nome) !== 'string' || typeof (idade) !== 'number') {
+  if (nome === undefined || idade === undefined) {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
