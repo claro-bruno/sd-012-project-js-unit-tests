1e2f88a0ae986330990e80c7ae2fbc79bb02beea
Augusto <augustocm2002@yahoo.com.br>
2021-05-24 22:21:17 -0300

    Fazendo requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const message = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+
+  return message;
+};
 
 module.exports = vqv;
+
+console.log(vqv('Augusto', 29));
