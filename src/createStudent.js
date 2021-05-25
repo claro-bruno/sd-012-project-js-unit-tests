dc21828251f9071f6fa002f17d6a3928a5c1af5a
Eder Santos <3derms@gmail.com>
2021-05-25 07:57:18 -0300

    fechando os parenteses na linha 23

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
+const createStudent = (nome => {
   return {
     name: `${nome}`,
     feedback: () => 'Eita pessoa boa!',
   };
-};
+})
 
 module.exports = createStudent;
