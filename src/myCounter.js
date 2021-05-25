bc3693a31bb14928d13eb8f3ebcb99bea7d86269
Eder Santos <3derms@gmail.com>
2021-05-25 10:32:11 -0300

    corrige variavel counter - linhas 22 e  23

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
