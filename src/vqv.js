9a01d52dc04c2a2e7cb491c4f3f0ce84547f3b5e
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-05-25 09:37:34 -0300

    Requisito 3 com lint

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof (nome) === 'undefined' && typeof (idade) === 'undefined') {
+    return undefined;
+  }
+  return (
+  `Oi, meu nome é ${nome}
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`);
+};
+console.log(vqv('Tunico', 30));
 
 module.exports = vqv;
