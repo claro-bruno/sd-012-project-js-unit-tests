d290af9dca8c31a157a15d95eede630f09647274
Pedro Resck <pedroresck1@gmail.com>
2021-05-25 15:31:40 -0300

    Requisito 3: resolvido - criar funcao vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === '' || typeof (idade) !== 'number') {
+    return undefined;
+  } return (`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
+};
 
+console.log(vqv('Tunico', 29));
 module.exports = vqv;
