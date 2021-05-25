1aefdcc3b7612b8052580c7783e013ecbe8ae689
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-05-25 15:01:14 -0300

    troca deepStrict por strictEqual

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -32,16 +32,16 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.deepStrictEqual(typeof (productDetails('Alcool gel', 'Mascara')), 'object');
+    assert.strictEqual(typeof (productDetails('Alcool gel', 'Mascara')), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.deepStrictEqual(productDetails('Alcool gel', 'Mascara').length, 2);
+    assert.strictEqual(productDetails('Alcool gel', 'Mascara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.deepStrictEqual(typeof (productDetails('Alcool gel', 'Mascara')[0]), 'object');
-    assert.deepStrictEqual(typeof (productDetails('Alcool gel', 'Mascara')[1]), 'object');
+    assert.strictEqual(typeof (productDetails('Alcool gel', 'Mascara')[0]), 'object');
+    assert.strictEqual(typeof (productDetails('Alcool gel', 'Mascara')[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notDeepStrictEqual(productDetails('Acool gel', 'Mascara')[0]), productDetails('Alcool gel', 'Mascara'[1]);
+    assert.notDeepStrictEqual(productDetails('Acool gel', 'Mascara')[0], productDetails('Alcool gel', 'Mascara')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.deepStrictEqual(productDetails('Alcool gel', 'Mascara')[0].details.productId.slice(-3), '123');
-    assert.deepStrictEqual(productDetails('Alcool gel', 'Mascara')[1].details.productId.slice(-3), '123');
+    assert.strictEqual(productDetails('Alcool gel', 'Mascara')[0].details.productId.slice(-3), '123');
+    assert.strictEqual(productDetails('Alcool gel', 'Mascara')[1].details.productId.slice(-3), '123');
   });
 });
