32f9ca6be630df7bf4503bee7d8c7893d3ac61a0
rachidb5 <66842598+rachidb5@users.noreply.github.com>
2021-05-25 18:26:40 -0300

    average funtion

--- src/average.js
+++ src/average.js
@@ -12,6 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (a, b) => (a + b) / 2;
+const average = (array) => {
+  let sum = 0;
+
+  if (array.length === 0) {
+    return undefined;
+  } for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    } sum += array[i];
+  } return sum / array.length;
+};
 
 module.exports = average;
