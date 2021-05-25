38b44851df7913283290bbe176be677d7b0ad411
Armando <a.guerra58@hotmail.com>
2021-05-24 21:43:48 -0300

    8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,10 +19,11 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter1 = 2; counter1 <= 3; counter += 1) {
-      myArray.push(counter1);
-    }
   }
+    for (let counter = 2; counter <= 3; counter += 1) {
+      myArray.push(counter);
+    }
+  
   return myArray;
 };
 
