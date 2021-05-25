c85ddaa561da4f900e3e7661d1bf20a0acfc2e70
David Azevedo <zevid.dev@outlook.com>
2021-05-24 21:22:28 -0300

    Done requirement 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {  
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    } 
+      sum += array[index] / array.length;
+  }
+  return Math.round(sum);
+};
 
 module.exports = average;
