7da568684cdaf2d6d959f1f68e1c1e124ac3b977
Abner <gedeaoabner@gmail.com>
2021-05-25 12:14:27 -0300

    adiciona função vqv com template literals - desafio 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const mensagem = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+
+  return mensagem;
+};
 
 module.exports = vqv;
