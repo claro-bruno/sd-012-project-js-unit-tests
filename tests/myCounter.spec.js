4a4b4dae5e5a996bf3915072826797d10477e321
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-24 21:25:48 -0300

    test push

--- tests/myCounter.spec.js
+++ tests/myCounter.spec.js
@@ -18,4 +18,6 @@ describe('8 - Implemente a função `myCounter`', () => {
     const expectedOutput = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
     assert.deepStrictEqual(myCounter(), expectedOutput);
   });
-});
+
+});  
+  
