551e6edb25809598d908de9cbbc49cd273120f97
jmlinger <jmlinger10@gmail.com>
2021-05-25 11:49:19 -0300

    resolve requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (num1, num2) => num1 + num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => Math.trunc(num1 / num2),
+  sub: (num1, num2) => num1 - num2,
+};
 
 module.exports = calculator;
