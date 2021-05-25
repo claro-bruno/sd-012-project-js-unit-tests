b55c9c8ccbf8eb7c69cbcf3d3e9b906f6a0e75bb
ladcs <hpladcs@gmail.com>
2021-05-25 15:09:46 -0300

    R9r2

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,8 +79,11 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => ({
-  fetchMenu: () => {},
+const createMenu = (objectIn) => ({
+  fetchMenu: (objectIn) => ({
+    food: {},
+    drink: {},
+  }),
 });
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -52,12 +52,14 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     assert.strictEqual(typeof createMenu().fetchMenu, 'function');
     // ```
-  
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
+    assert.notStrictEqual(typeof createMenu({food: {}, drink:{}}).fetchMenu({food: {}, drink:{}}).food, 'undefined');
+    assert.notStrictEqual(typeof createMenu({food: {}, drink:{}}).fetchMenu({food: {}, drink:{}}).drink, 'undefined');
     // ```
+    
     // const objetoRetornado = createMenu({ food: {}, drink: {} });
     // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
     // ```
