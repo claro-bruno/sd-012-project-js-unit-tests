dbae230add2933e3d3f7eece5dd427c3ec40b6ae
jmlinger <jmlinger10@gmail.com>
2021-05-24 22:11:39 -0300

    resolve requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => ({
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  });
 
+// const estudante = createStudent('Leandrão, o Lobo Solitário');
+// console.log(estudante.name);
+// console.log(estudante.feedback());
 module.exports = createStudent;
