b912097aa322fe35eb0a61baa728278777ba1b59
Eder Santos <3derms@gmail.com>
2021-05-25 09:02:05 -0300

    inclusao de testes productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails('firstProduct', 'secondProduct'), 'object')
     // Teste que o array retornado pela função contém dois itens dentro.
+    //assert.strictEqual(Object.keys(productDetails('firstProduct', 'secondProduct')), '['0', '1']')
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara'), [ { name: 'Alcool gel', details: { productId: 'Alcool gel123' } },
+    { name: 'Máscara', details: { productId: 'Máscara123' } } ])
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
+
+//let array = {a: 1, b: 2,};
+//console.log(Object.keys(array))
+//console.log(Object.keys(productDetails()))
+//console.log(productDetails('Alcool gel', 'Máscara'))
+
+
