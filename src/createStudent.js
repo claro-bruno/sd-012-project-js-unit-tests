31d418443caacebd1f54c1aa2906b49e0b77d8e7
Patricia Silvestre <patriciasilvestrea@gmail.com>
2021-05-25 11:02:31 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+   const estudante = {
+     name: nome,
+     feedback: () => 'Eita pessoa boa!',
+   };
+   return estudante;
+  };
+  console.log(createStudent('Leandrão, o Lobo Solitário').name);
+  console.log(createStudent().feedback());
 module.exports = createStudent;
