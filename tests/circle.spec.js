53a011fb605cedf3864665ce39c2e16f5ba8e117
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-25 11:47:16 -0300

    Adiciona testes para requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const assert = require('assert');
+const { randomInt } = require('crypto');
 const circle = require('../src/circle');
 
 /*
@@ -25,7 +26,14 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    
+    assert.strictEqual(typeof (circle(Math.floor(Math.random() * 1001))), 'object');
+    let randomCircle = circle(Math.floor(Math.random() * 1001));
+    assert.strictEqual(Object.entries(randomCircle).length, 3);
+    assert.strictEqual(circle(), undefined);
+    assert.strictEqual(circle(2).circumference, 12.56);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
+    let circleRadius3 = { radius: 3, area: 28.259999999999998, circumference: 18.84 };
+    assert.deepStrictEqual(circle(3), circleRadius3);
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
