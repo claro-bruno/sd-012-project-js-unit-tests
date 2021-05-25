c805e0a997a2481bf7a3c64f0ca6c7cd8830e174
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-24 21:46:30 -0300

    Arruma linha 20 do requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,7 @@
 */
 
 const vqv = (nome, idade) => {
-  let presentation =   `Oi, meu nome é ${nome}!
+  let presentation = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
