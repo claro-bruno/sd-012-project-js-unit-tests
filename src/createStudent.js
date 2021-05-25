e28923f76687155d74c1b9f9ba79c414cc90e5ec
Eder Santos <3derms@gmail.com>
2021-05-25 07:45:43 -0300

    Corrige a linha 21

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,9 +18,7 @@
 const createStudent = (nome) => {
   return {
     name: `${nome}`,
-    feedback: () => {
-      return ('Eita pessoa boa!');
-    },
+    feedback: () => 'Eita pessoa boa!',
   };
 };
 
