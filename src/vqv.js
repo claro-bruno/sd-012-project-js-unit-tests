e6f8663c9537fd13a3b92fb611897625603dae0b
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-05-26 18:32:59 -0300

    resolve lint requesito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,12 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof nome == 'undefined' && typeof idade == 'undefined') {
+  if (typeof nome === 'undefined' && typeof idade == 'undefined') {
     return undefined;
   }
-
   return`Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
-  
 };
 module.exports = vqv;
