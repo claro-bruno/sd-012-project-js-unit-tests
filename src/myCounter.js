62b2fb68625fb8775fbc911e5d37a9699328adfd
Ed Santos <edsantos2822@gmail.com>
2021-05-26 00:30:09 -0300

    Finalizando - Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,16 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
