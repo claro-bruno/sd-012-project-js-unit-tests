340a9124dafecc7932b6bfc8664d38eb49c258b9
Julio Pereira <jandrade.pereira1@gmail.com>
2021-05-26 16:10:22 -0300

    Corrige o requisito 4 ao retirar o parseFloat na funcao e insere a resposta do requisito 5

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -32,11 +32,12 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    
     assert.strictEqual(typeof circle(2), 'object');
     assert.strictEqual(Object.keys(circle(20)).length, 3);
     assert.strictEqual(circle(), undefined);
-    assert.strictEqual(circle(2).circumference, 13);
-    assert.strictEqual(circle(3).area, 28);
-    assert.deepStrictEqual(circle(3), {"radius": 3, "area": 28, "circumference": 19});
+    assert.strictEqual(circle(2).circumference, 12.56);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
+    assert.deepStrictEqual(circle(3), {"radius": 3, "area": 28.259999999999998, "circumference": 18.84});
   });
 });
