377d9e8cff4d5d4f47e11f073449a81ac27b2fb6
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-26 19:17:08 -0300

    Requirement 06-> Add test to check if array items are objects

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,6 +36,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('Alcool', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(productDetails('Alcool', 'Mácara').every((element) => (typeof element) === 'object'), true);
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
