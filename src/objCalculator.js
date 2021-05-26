d5d95097402857b8f358688983014adb5e5aa7d6
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-26 11:34:08 -0300

    Insere semicolon linha 27

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -24,6 +24,6 @@ const calculator = {
     mult: (num1, num2) => num1 * num2,
     div: (num1, num2) => Math.floor(num1 / num2),
     sub: (num1, num2) => num1 - num2,
-  }
+  };
 
 module.exports = calculator;
