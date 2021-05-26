18c9adc818352b6fbaefe3030120cc5334ee324d
Gabriel Viana <g.viana87@hotmail.com>
2021-05-26 20:49:22 -0300

    item 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -20,7 +20,7 @@ const createStudent = (nome) => {
     name: nome,
     feedback: () => 'Eita pessoa boa!',
   };
-  return estudante
+  return estudante;
 };
 
 // console.log(createStudent('Leandrão, o Lobo Solitário'))
