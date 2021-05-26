9c4658c226500d02758469c58c6e5d4945ac7661
Caio-Veloso <caio.veloso@gmail.com>
2021-05-25 21:14:34 -0300

    Requisito 2 testado localmente

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert');
-const numbers = require('../src/numbers');
+const assert = require("assert");
+const numbers = require("../src/numbers");
 
 /*
   A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
@@ -16,9 +16,13 @@ const numbers = require('../src/numbers');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    assert.fail();
+describe("2 - Implemente os casos de teste para a função `numbers`", () => {
+  it("Verifica se a função `numbers`retorna um array e se o array retornado contém somente números", () => {
+    //assert.fail();
+    assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
+    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
+    assert.deepStrictEqual(numbers([1, 'a', 3]), false);
+    assert.deepStrictEqual(numbers([' ']), false);
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
