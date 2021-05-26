cfdc1a3cc4dde01fa3400b2a70b4c1aae296db68
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-26 12:35:36 -0300

    Requirement 06-> Add test to check if the function `productDetails` return an array with two items

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,6 +34,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que o retorno da função é um array.
     assert.strictEqual(Array.isArray(productDetails('Alcool', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
