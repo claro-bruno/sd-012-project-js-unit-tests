adae5b49ce2763dedb15de1cbf7b6999bb61f9b2
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-26 11:48:31 -0300

    Troca variavel linha 22 requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let count = 2; count <= 3; count += 1) {
+      myArray.push(count);
     }
   }
   return myArray;
