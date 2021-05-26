4037205b9c1789e08bbdfe861d7b421ff46bf679
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-05-25 22:52:02 -0300

    add requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let result;
+  if (nome !== undefined) {
+    result = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  } 
+return result;
+};
 
 module.exports = vqv;
