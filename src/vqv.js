705cdc07d204d9459e3b2a7bd1015b3b4aa4f4e4
Sass <dudu_sass12@hotmail.com>
2021-05-25 21:23:49 -0300

    adicionando !

--- src/vqv.js
+++ src/vqv.js
@@ -21,7 +21,7 @@ const vqv = (nome, idade) => {
  return undefined;
  }
 
- return `Oi, meu nome é ${nome}
+ return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
