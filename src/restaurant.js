22b7e2c59b522ab525f18aebf0277287989a1882
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-05-26 16:24:23 -0300

    Requisito 9 - Acerta variavel meuRestaurante para passar no ESLint

--- src/restaurant.js
+++ src/restaurant.js
@@ -78,11 +78,7 @@
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`,
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso,
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
-
-const meuRestaurante = createMenu({
- food: { coxinha: 3.9, sopa: 9.9 },
- drink: { agua: 3.9, cerveja: 6.9 },
-});
+let meuRestaurante;
 
 const orderFunction = (order) => {
  meuRestaurante.consumption.push(order);
@@ -108,6 +104,11 @@ const createMenu = (object) => ({
   consumption: [],
   order: orderFunction,
   pay: payFunction,
+});
+
+meuRestaurante = createMenu({
+ food: { coxinha: 3.9, sopa: 9.9 },
+ drink: { agua: 3.9, cerveja: 6.9 },
 });
 
 module.exports = { createMenu, meuRestaurante };
