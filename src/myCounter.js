e40dd6cebd4dca4db63b872f343254b3ec1c1e50
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-05-26 09:00:38 -0300

    Requisito 8 com lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,15 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    
+    for (let contador = 2; contador <= 3; contador += 1) {
+      myArray.push(contador);
     }
   }
-  return myArray;
-};
-
+    return myArray;
+  };
+  
 module.exports = myCounter;
