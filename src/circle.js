cdee7500bd52b384d796a7b0f71ec138455b3f9b
Raphael <raphael.ph2ll@gmail.com>
2021-05-26 12:51:09 -0300

    Refatorando ex04 - circle

--- src/circle.js
+++ src/circle.js
@@ -23,9 +23,9 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: parseFloat((PI * radius * radius).toPrecision(2)),
-    circumference: parseFloat((2 * PI * radius).toPrecision(2)),
+    area: PI * radius * radius,
+    circumference: 2 * PI * radius,
   };
 };
-
+console.log(circle(3));
 module.exports = circle;
