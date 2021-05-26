d9b02498f4ffaa6e29b7b77b44a70c0607741453
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-05-26 13:02:05 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -15,7 +15,14 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof (nome) === 'string' && typeof (idade) === 'number') {
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  }
+  return undefined; 
+};
 
 module.exports = vqv;
