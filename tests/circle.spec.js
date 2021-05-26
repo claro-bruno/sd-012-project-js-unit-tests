c6127af11d996fae4f35c0f86b892ca792fa1125
David Azevedo <zevid.dev@outlook.com>
2021-05-26 15:33:54 -0300

    Correction requirement 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -38,7 +38,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).area.toPrecision(4), 28.26);
+    assert.strictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), {radius: 3, area: 3 * 3.14 * 3, circumference: 2 * 3.14 * 3});
   });
