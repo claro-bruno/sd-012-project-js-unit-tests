b01e2b242c9e6b1eedbb372ab134665426c03867
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-05-27 12:12:43 -0300

    resolve requesito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (name) => {
+  const estudante = {
+    name: name,
+    feedback: () => 'Eita pessoa boa!'
+  }
+  return estudante;
+};
 module.exports = createStudent;
