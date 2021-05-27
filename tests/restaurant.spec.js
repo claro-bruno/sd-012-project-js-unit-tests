0dc190026d90d4a2463ba1b0b776a6c8ca4c9911
Marcela Silva <marcela.ops@hotmail.br>
2021-05-27 12:43:34 -0300

    requisito 9

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -48,7 +48,7 @@ const createMenu = require('../src/restaurant');
 
 describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     // ```
