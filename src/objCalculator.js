d241bdc57c37623611ad1ff5cd11876d8ac1d358
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-27 20:25:48 -0300

    ajustes no lint do 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,18 +20,13 @@
 */
 
 const calculator = {
-  add: (num1, num2) => {
-    return num1 + num2;
-  },
-  mult: (num1, num2) => {
-    return num1 * num2;
-  },
-  div: (num1, num2) => {
-    return Math.floor(num1 / num2);
-  },
-  sub: (num1, num2) => {
-    return num1 - num2;
-  },
+  add: (num1, num2) => num1 + num2,
+
+  mult: (num1, num2) => num1 * num2,
+
+  div: (num1, num2) => Math.floor(num1 / num2),
+
+  sub: (num1, num2) => num1 - num2,
 };
 
 module.exports = calculator;
