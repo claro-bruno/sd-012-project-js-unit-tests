f59fc2d97b7cdaa46594b887e322ff6db9dcfa69
Emmanuel <acquabriall2020@gmail.com>
2021-05-27 14:57:01 -0300

    resolve o req 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  const um = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\n`;
+  const dois = 'trabalho na Trybe e mando muito em programação!\n#VQV!';
+  const final = um + dois;
+
+  return final;
+};
 
 module.exports = vqv;
