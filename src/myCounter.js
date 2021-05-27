47bc53048d8d3ae33159f20afd28e0b8b26fe827
walttoncoelho <walltton@outlook.com>
2021-05-27 11:26:47 -0300

    Resolve a questão 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,12 +20,12 @@ const myCounter = (retorna) => {
 
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
-//console.log(myCounter(myArray));
+// console.log(myCounter(myArray));
 
 module.exports = myCounter;
