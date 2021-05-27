7c33f5139eb6b4966b6cd348ab36c935b3b69a0d
Gabriel Viana <g.viana87@hotmail.com>
2021-05-27 19:19:17 -0300

    item 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -25,5 +25,9 @@ const calculator = {
   div: (n1, n2) => Math.floor(n1 / n2),
   sub: (n1, n2) => (n1 - n2),
 };
+console.log(calculator.add(1, 3))
+console.log(calculator.mult(10, 3))
+console.log(calculator.div(5, 2))
+console.log(calculator.sub(1, 3))
 
 module.exports = calculator;
