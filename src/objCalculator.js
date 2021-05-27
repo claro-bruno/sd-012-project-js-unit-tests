59a68389358edcca1fac35563a9182f65da19710
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-27 11:53:34 -0300

    Requirement 07-> Add function `sub` on the object `calculator`

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -23,6 +23,7 @@ const calculator = {
   add: (number1, number2) => number1 + number2,
   mult: (number1, number2) => number1 * number2,
   div: (number1, number2) => Math.floor(number1 / number2),
+  sub: (number1, number2) => number1 - number2,
 };
 
 module.exports = calculator;
