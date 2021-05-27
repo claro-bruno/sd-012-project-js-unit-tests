8d70e3cb487df62c46cbf727ad197b897efd5742
Sass <dudu_sass12@hotmail.com>
2021-05-27 15:29:38 -0300

    exercicio 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+  const feedback = () => 'Eita pessoa boa!';
+  const person = {
+    name: nome,
+    feedback: feedback(),
+  };
+  return person;
+};
+console.log(createStudent('Dudu'));
 module.exports = createStudent;
