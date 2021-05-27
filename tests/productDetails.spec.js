d5fe62c7408269be5da68c021fc745589f3a9d37
Matheus Proenca <mproenca.marketing@gmail.com>
2021-05-27 10:13:31 -0300

    resolvendo conflito 3

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,10 +37,12 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof (productDetails('Alcool gel', 'Máscara')[0, 1]), 'object');
+    assert.strictEqual(typeof (productDetails('Alcool gel', 'Máscara')[0]), 'object');
+    assert.strictEqual(typeof (productDetails('Alcool gel', 'Máscara')[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.notStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
     // Teste que os dois productIds terminam com 123.
-    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara')[0, 1].details.productId.match(/123$/g), ['123']);
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara')[0].details.productId.match(/123$/g), ['123']);
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara')[1].details.productId.match(/123$/g), ['123']);
   });
 });
