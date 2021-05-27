be502f30da9d18d87871cf8a7bb694ba51e1aef1
ViniciusMossi <vini_agg@hotmail.com>
2021-05-26 23:33:02 -0400

    verifica novamente o erro

--- src/vqv.js
+++ src/vqv.js
@@ -20,8 +20,8 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   } 
-  return `Oi, meu nome é${nome}!
-Tenho ${idade}anos,
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
 };
