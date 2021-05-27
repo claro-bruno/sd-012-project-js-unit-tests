9204b16149c21c9fdb81b3185a781bc2ae13d968
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-05-27 12:24:00 -0300

    resolvendo lint  requesito 5 1.1

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (nome) => {
   const estudante = {
     name: nome,
-    feedback() { 'Eita pessoa boa!' };
+    feedback: () => 'Eita pessoa boa!',
   };
   return estudante;
 };
