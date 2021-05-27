f3a2fafd9d184ba40063c620cf8f02e70d8eae78
martinsph <ph.mmartins@gmail.com>
2021-05-27 18:15:48 -0300

    adiciona req 8

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (a, b) => Math.trunc(a + b),
+  mult: (a, b) => Math.trunc(a * b),
+  div: (a, b) => Math.trunc(a / b),
+  sub: (a, b) => Math.trunc(a - b),
+};
 
 module.exports = calculator;
