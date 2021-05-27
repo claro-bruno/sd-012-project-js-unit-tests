2304f37c01e96db4984ef4af77d7678d1f0f6cbf
walttoncoelho <walltton@outlook.com>
2021-05-27 11:11:40 -0300

    Resolve a questão 7

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,15 +15,17 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+const myCounter = (retorna) => {
+  let myArray = [];
+
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
+//console.log(myCounter(myArray));
 
 module.exports = myCounter;
