af6e05027289f256bfd11f619050aff8e66842ec
MaxAlves0035 <maxalves2@hotmail.com>
2021-05-27 10:21:14 -0300

    requisito 4 - edit

--- src/circle.js
+++ src/circle.js
@@ -23,7 +23,7 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: PI * radius * radius,
+    area: parseFloat((PI * radius * radius).toPrecision(2)),
     circumference: 2 * PI * radius,
   };
 };
