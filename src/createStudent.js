62bdca71ad03e7b69d050cd7cf2341c3492e5809
thales <thalesdaiel2013@gmail.com>
2021-05-26 21:44:46 -0300

    feito alteraoes no lint

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,9 +14,10 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const createStudent = (nome) => (
-    name: nome, 
-    feedback: () => 'Eita pessoa boa!',
-);
-console.log(createStudent('thales').feedback());
+const createStudent = (nome) => {
+  let obj = { name: nome, 
+  feedback: () => 'Eita pessoa boa' };
+  return obj;
+};
+
 module.exports = createStudent;
