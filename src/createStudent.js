79caa8d790d81502c33be63c2a99362a8c50a15f
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-05-27 20:45:11 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+ const estudante = {
+    name,
+    feedback: () => 'Eita pessoa boa!',  
+ };
+  return estudante;
+};
 
 module.exports = createStudent;
