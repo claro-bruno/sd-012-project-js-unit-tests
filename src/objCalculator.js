21f95482b6db58aff6ce9b61987d38b78bed4657
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-27 20:16:59 -0300

    requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,19 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (num1, num2) => {
+    return num1 + num2;
+  },
+  mult: (num1, num2) => {
+    return num1 * num2;
+  },
+  div: (num1, num2) => {
+    return Math.floor(num1 / num2);
+  },
+  sub: (num1, num2) => {
+    return num1 - num2;
+  },
+};
 
 module.exports = calculator;
