b902c8f924a1a9505dff47293a2cb9d60fb31823
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-27 19:46:14 -0300

    tentando resolver conflict

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -42,10 +42,16 @@ const checkIfObj = (obj) => {
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
     assert.strictEqual(Array.isArray(productDetails('a', 'b')), true);
+    // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('a', 'b').length, 2);
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
     assert.strictEqual(typeof (productDetails('a', 'b').values()), 'object');
+    // Teste que os dois objetos são diferentes entre si.
     assert.strictEqual(product[0] === product[1], false);
+    // Teste que os dois productIds terminam com 123.
     assert.strictEqual(product[0].details.productId.substr(-3) === product[0].details.productId.substr(-3), true);
   });
 });
