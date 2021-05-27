c0dd645c4cbe86a049c0412070118c86959fc40a
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-05-27 00:18:25 -0300

    Concluido requisito 2

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert');
-const circle = require('../src/circle');
+const assert = require("assert");
+const circle = require("../src/circle");
 
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -23,15 +23,17 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 entradas.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+describe("4 - Implemente os casos de teste para a função `circle`", () => {
+  it("Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados", () => {
+    assert.strictEqual(typeof circle(1), "object");
+    assert.strictEqual(Object.entries(circle(3)).length, 3);
+    assert.strictEqual(circle(), undefined);
+    assert.strictEqual(circle(2).circumference, 12.56);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
+    assert.deepStrictEqual(circle(3), {
+      area: 28.259999999999998,
+      circumference: 18.84,
+      radius: 3,
+    });
   });
 });
