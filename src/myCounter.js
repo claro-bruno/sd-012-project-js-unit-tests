ac19f6dcb8cc62af8ca4ffd121b48a1b94448128
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-27 12:12:02 -0300

    Requirement 08-> Correctiong the loops in the function `myCounter`

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,9 +17,9 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
