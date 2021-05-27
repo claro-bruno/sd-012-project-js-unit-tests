d49e712c86c00c03770f48faa3b7f0cff845007d
Eicke Yumoto <eickeyumoto@gmail.com>
2021-05-27 14:32:08 -0300

    avarage

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let result = 0;
+  if (array.length === 0 || array === undefined) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    result += array[index];
+  }
+
+  result = Math.round(result / array.length);
+  return result;
+};
 
 module.exports = average;
