c07dff5067b2bfb1a6f35725e694023818d43b2b
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-27 19:02:25 -0300

    Resolve erro requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -41,11 +41,9 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que os dois objetos são diferentes entre si.
  Ruan-unit-tests
     assert.notDeepStrictEqual(productDetails('Alcool gel', 'Mascara')[0], (productDetails('Alcool gel', 'Mascara')[1]))
-    // (Difícil) Teste que os dois productIds terminam com 123
+    // Teste que os dois productIds terminam com 123
     assert.deepStrictEqual(productDetails('Alcool gel', 'Mascara')[0].details.productId.endsWith('123'), true);
     assert.deepStrictEqual(productDetails('Alcool gel', 'Mascara')[1].details.productId.endsWith('123'), true);
-
-    // Teste que os dois productIds terminam com 123.
-
+    
   });
 });
