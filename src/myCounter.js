6ca27ce775a6c91f82dc8a8f507b8bdd4ac16ff3
Sass <dudu_sass12@hotmail.com>
2021-05-27 17:58:09 -0300

    exercicio 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let i = 2; i <= 3; i += 1) {
+      myArray.push(i);
     }
   }
   return myArray;
 };
-
+console.log(myCounter(2));
 module.exports = myCounter;
