1ca4abbd32659d42f0b8e184960766bc2e07f223
Pedro Resck <pedroresck1@gmail.com>
2021-05-27 13:23:55 -0300

    Requisito 7: resolvido - criar a funcao objCalulator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -18,7 +18,15 @@
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
+const soma = (num1, num2) => num1 + num2;
+const multiplicacao = (num1, num2) => num1 * num2; 
+const divisao = (num1, num2) => Math.trunc(num1 / num2);
+const subtracao = (num1, num2) => num1 - num2;
 
-const calculator = {};
-
+const calculator = {
+    add: soma,
+    mult: multiplicacao,
+    div: divisao,
+    sub: subtracao,
+  };
 module.exports = calculator;
