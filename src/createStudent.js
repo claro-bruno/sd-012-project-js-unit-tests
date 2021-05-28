ffe42b8d5575fffcc707a984d5e2935f8e4a04be
Eurides <euridesalves@yahoo.com.br>
2021-05-28 12:08:09 -0300

    adicionado requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (studentName) => {
+  const arrayStudent = { 
+    name: studentName,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return arrayStudent;
+};
 
 module.exports = createStudent;
