d799320ba92ef46159b8c98fae56b935d46159cd
ViniciusMossi <vini_agg@hotmail.com>
2021-05-28 17:59:22 -0400

    verifica requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,10 +20,11 @@ const myCounter = () => {
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter);
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
+console.log((myCounter()));
 
 module.exports = myCounter;
