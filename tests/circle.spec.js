9daafa17f63104512a9168cb0c61daf74c8716be
Juliane Marques <mqs.juliane@gmail.com>
2021-05-28 10:49:17 -0300

    envia circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,10 +25,10 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.strictEqual(typeof circle(3, 'object'));
+    assert.strictEqual(typeof circle(3), 'object');
     // Teste se o objeto retornado tem 3 entradas.
     assert.deepStrictEqual(Object.keys(circle(3)), ["radius", "area", "circumference"]);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
