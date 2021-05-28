8f5d9f84c0b8232cb07183712a561811a8618bea
Gabriel Viana <g.viana87@hotmail.com>
2021-05-27 22:00:42 -0300

    item 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
