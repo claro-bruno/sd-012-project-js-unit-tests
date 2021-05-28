7c03c3e6b6f8541f3fa729dbb7d97ce3b72d10a2
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-28 11:42:38 -0300

    Requirement 09-> Add key `fetchMenu` in the object returned by the function `createMenu`

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -48,12 +48,12 @@ const createMenu = require('../src/restaurant');
 
 describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
+    assert.strictEqual((typeof createMenu().fetchMenu), 'function');
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
