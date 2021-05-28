c5de76894df8ab91de3fd0eef7d248055fa4cca0
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-05-28 12:20:24 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,13 +30,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    
+    //const retornodaFuncao = productDetails('Alcool gel', 'Máscara');
+    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel','Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Alcool gel', 'Mascara')[0],productDetails('Alcool gel', 'Mascara')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    //assert.strictEqual(arr.every((item) => {item.details.productId.substr(-3) }), true);
   });
 });
