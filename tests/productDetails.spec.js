890d5b448e629b8ab8d3321cf947a4414ff5ba2e
ViniciusMossi <vini_agg@hotmail.com>
2021-05-27 22:32:00 -0400

    verifica requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,15 +27,20 @@ const productDetails = require('../src/productDetails');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-
+// **AUXILIO notStrictEqual ** > https://www.w3schools.com/nodejs/met_assert_notstrictequal.asp
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails(), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails()[0], productDetails()[1]);
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual()
   });
 });
