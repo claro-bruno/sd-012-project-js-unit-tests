f7fc9d057321aafd3e9c54cd1523848428086929
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-27 21:20:58 -0300

    corrige outros lint

--- src/average.js
+++ src/average.js
@@ -14,15 +14,12 @@
 
 const average = (array) => {
 let sum = 0;
-
-if (array.length === 0) {
-  return undefined;
-} for (let index = 0; index < array.length; index += 1){
-  if ( typeof (array[index]) !== 'number') {
-  return undefined;
-} sum += array[index]; 
-return Math.round( sum / array.length);
+if (array.length === 0) return undefined;
+for (let index = 0; index < array.length; index += 1) {
+  if (typeof (array[index]) === 'string') return undefined;
+  sum += array[index]; 
 }
+return Math.round(sum / array.length);
 };
 
 module.exports = average;
