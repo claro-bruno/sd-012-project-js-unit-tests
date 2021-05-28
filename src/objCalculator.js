d7c725c1fbe17ae84fbf69c994cd001b30b1e6ce
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-05-28 13:15:08 -0300

    add requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -18,7 +18,11 @@
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
-
-const calculator = {};
+const calculator = {
+  add: (num1, num2) => num1 + num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => Math.floor(num1 / num2),
+  sub: (num1, num2) => num1 - num2,
+};
 
 module.exports = calculator;
