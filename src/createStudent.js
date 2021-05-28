f2db8b4014647ab613c07dd515cf770dd68b8a14
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-28 16:20:46 -0300

    createStudent finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+  const object = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return object;
+};
 module.exports = createStudent;
