2efea3418fbf4e1bff19e8d055c7124b1dcbd937
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-28 16:05:11 -0300

    average finalizado

--- src/average.js
+++ src/average.js
@@ -11,7 +11,18 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
-const average = () => {
-
+const average = (array) => {
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    } 
+      soma += array[index];
+  }
+  soma /= array.length;
+  return Math.round(soma);
+};
 module.exports = average;
