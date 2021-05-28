1ab3669ceb9aaf610ada62e1fb4a2e38be0445ea
ViniciusMossi <vini_agg@hotmail.com>
2021-05-27 21:38:17 -0400

    verifica requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const estudante = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+    };
+    return estudante;
+  };
+    
+
+  }
+};
 
 module.exports = createStudent;
