b393f6bebed09b001ea7bb7f1964d8ccf68b0544
Wadson <wadson.dev92@gmail.com>
2021-05-29 01:13:01 -0300

    identaçao ok funçao 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,7 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => ({name: nome, feedback: () => 'Eita pessoa boa!'
+const createStudent = (nome) => ({
+  name: nome,
+  feedback: () => 'Eita pessoa boa!'
 });
 
 createStudent('Wadson');
