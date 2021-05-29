1a41374cd548037964658f0b8e2fa5428ab12799
gscuri <gscuri6@hotmail.com>
2021-05-29 16:43:44 -0300

    resolucao exercicio 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,7 +19,7 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
