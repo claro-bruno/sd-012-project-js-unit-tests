d6c8d758e62b58ae2522e1225ede5519d8f9ad1a
Juliane Marques <mqs.juliane@gmail.com>
2021-05-29 16:45:37 -0300

    como resolver conflito? ainda nao sei

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -25,14 +25,14 @@ const productDetails = require('../src/productDetails');
     }
   ]
 
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
+    OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 const teste1 = Array.isArray(productDetails());
 const teste2 = productDetails('produto1', 'produto2');
 
-
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     assert.strictEqual(teste1, true);
@@ -42,9 +42,8 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     assert.strictEqual(typeof teste2.values(), 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.strictEqual(Object.values(teste2[0]) !== Object.values(teste2[1]), true);
-     // Teste que os dois productIds terminam com 123.
+    // (Difícil) Teste que os dois productIds terminam com 123.
     assert.strictEqual(teste2[0].details.productId.substr(-3), '123');
     assert.strictEqual(teste2[1].details.productId.substr(-3), '123');
   });
 });
-
