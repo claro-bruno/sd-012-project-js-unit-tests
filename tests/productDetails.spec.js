4f1cc848e4e08887ed4b0c0a41245c0cd7409f5d
RenanMiyashita <renanmiyashita@gmail.com>
2021-05-29 15:50:27 -0300

    req 6.2

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -39,7 +39,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara')[0], 'object');
     assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara')[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
+    assert.notStrictEqual(productDetails('Alcool gel', 'Máscara')[0, 1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
