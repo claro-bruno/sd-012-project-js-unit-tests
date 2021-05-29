708f4796461be0be6583dd1c35e6636686fc056a
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-29 11:49:48 -0300

    requisito 8 feito

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,14 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  for (var index = 0; index <= 3; index += 1) {
+    myArray.push(index);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
+console.log(myCounter());
 
 module.exports = myCounter;
