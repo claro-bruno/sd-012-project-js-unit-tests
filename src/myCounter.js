052ea67311fb363032f71776557018399416edef
julio11lp <julio11lp@hotmail.com>
2021-05-29 18:37:08 -0300

    Criterio 8 aprovado no lint e test

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,16 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let index = 2; index <= 3; index += 1) {
+      myArray.push(index);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
