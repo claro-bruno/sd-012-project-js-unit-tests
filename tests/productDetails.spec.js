43add618601a312b103488886cd9a217a9f5fca7
julio11lp <julio11lp@hotmail.com>
2021-05-29 17:29:28 -0300

    Criterio 6 aprovado no test e lint

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,20 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true );
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    const object1 = Object.values(productDetails('Alcool gel', 'Máscara')[0]);
+    const object2 = Object.values(productDetails('Alcool gel', 'Máscara')[1]);
+    assert.strictEqual(object1 === object2, false);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    // Codigo inspirado no git do Gabriel Bueno
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0].details.productId.substr(-3), '123');
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[1].details.productId.substr(-3), '123');
   });
 });
