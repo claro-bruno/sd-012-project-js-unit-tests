99cee75b49a1e9a0a1247505302bf72f903e8480
Victor Veloso <victorveloso13@gmail.com>
2021-05-29 20:46:47 -0300

    Faz requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome && idade) {
+    return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  }
+  return undefined;
+};
+console.log(vqv('Victor', 26));
 
 module.exports = vqv;
