381163a94e1f562510a9c2574069243ca0eecb5b
Juliane Marques <mqs.juliane@gmail.com>
2021-05-29 15:09:00 -0300

    testando 02

--- src/vqv.js
+++ src/vqv.js
@@ -22,7 +22,7 @@ const vqv = (nome, idade) => {
   }
 
   return `Oi, meu nome é ${nome}!\n`
-+ `Tenho ${idade} anos,\n`
++ `Tenho ${idade}anos,\n`
 + 'trabalho na Trybe e mando muito em programação!\n'
 + '#VQV!';
 };
