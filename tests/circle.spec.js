ef4b1707badfada9acabb9f3f64d1f6f31ad329f
Aldria Melo <aldriamelo.academico@gmail.com>
2021-05-29 15:42:49 -0300

    iniciando projeto

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,7 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
+
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
