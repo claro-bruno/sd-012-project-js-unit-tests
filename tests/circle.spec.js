bf6fd743d32c712ac0cda2d55ff3bbfd825d48e6
Pedro Pinheiro <ph.santos.pinheiro@hotmail.com>
2021-05-28 23:12:22 -0300

    corrigindo erro da avaliaçao tentativa

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,22 +25,22 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    //assert.fail();
+    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-       // assert(circle(1)) sucesso !!
+       
     
     // Teste se o objeto retornado tem 3 entradas.
-       //assert(circle()) 
+      
  
        // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-        //assert.strictEqual(circle()) 
+        
     
         // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-          //assert.strictEqual(circle()) 
+          
       
           // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-            //assert.strictEqual(circle())
+            
         
             // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
