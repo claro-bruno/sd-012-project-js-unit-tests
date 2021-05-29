ad6b8d530e39a4035ed12a47d55be62a5b51a5db
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-29 13:20:09 -0300

    correcao req myCOunter

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,7 +20,7 @@ const myCounter = () => {
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter);
+      myArray.push(counter2);
     }
   }
   return myArray;
