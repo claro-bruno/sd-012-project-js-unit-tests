3fbe35460b09b7c9f1ed46b8b138ef44f9e5a360
Anderson Barbosa <andbmarques@gmail.com>
2021-05-29 13:51:08 -0300

    Requisito 5 adicionado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const estudante = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
