9657aee8c4571c47d49c31912d0e19dca7e5cbd4
Gabriela-GUerra <gabrielaguerra.arq@gmail.com>
2021-05-29 20:51:07 -0300

    adiciona correçao de lint requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -21,8 +21,8 @@ const vqv = (nome, idade) => {
     return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
-  };
+#VQV!`;
+  }
   return undefined;
 };
 
