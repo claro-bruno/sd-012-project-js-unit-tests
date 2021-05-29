b0d164ae171f971e94895ed8103f25f7c00d2ec7
RenanMiyashita <renanmiyashita@gmail.com>
2021-05-29 16:23:10 -0300

    req 8 e req 7.3

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  let myArray = [];
+  for (let counter1 = 0; counter1 <= 3; counter1 += 1) {
+    myArray.push(counter1);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
