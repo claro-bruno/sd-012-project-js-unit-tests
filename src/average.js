66e94a9346817fd7cb42d02b8ef1ae9445ccbef1
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-05-29 12:59:24 -0300

    resolve primeiro teste

--- src/average.js
+++ src/average.js
@@ -12,6 +12,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let counter = 0;
+  if (array.length === 0) return undefined;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') return undefined;
+    counter += array[index];
+  }
+  return Math.round(counter / array.length);
+};
 
 module.exports = average;
