dbf2bb430e27437f17d21c40a5edd0d8eb5865fa
Martin <escorpmartin97@gmail.com>
2021-05-28 22:47:16 -0300

    req 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const assert = require('assert');
+//const { assert } = require('console');
 const circle = require('../src/circle');
 
 /*
@@ -29,17 +30,16 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
 
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(5), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.strictEqual(typeof circle.return, 'object');
     assert.strictEqual(circle (circle.return, {radius: 1, area: 3.14, circumference: 6.28}));
-    assert.strictEqual(circle.radius === '', undefined);
-    assert.strictEqual(circle (circle.return.circumference, {radius: 3, area: 3.14, circumference: 6.28}));
-    // assert.strictEqual(circle(circumference === 3))
-
-
+    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle() , undefined);
+    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(parseFloat(circle(2).circumference.toPrecision(2)), 13);
+    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(parseFloat(circle(3).area.toPrecision(2)), 28);
+    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.259999999999998, circumference: 18.84});
   });
 });
