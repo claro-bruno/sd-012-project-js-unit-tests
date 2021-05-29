6c583d813e33bd8ba17d38085655b83410a12363
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-29 13:44:30 -0300

    correcao lint req 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,10 +20,10 @@
 */
 
 const calculator = {
-  add: (x,y) => x + y,
-  mult: (x,y) => x * y,
-  div: (x,y) => Math.floor(x / y),
-  sub: (x,y) => x - y,
+  add: (x, y) => x + y, 
+  mult: (x, y) => x * y, 
+  div: (x, y) => Math.floor(x / y), 
+  sub: (x, y) => x - y, 
 };
 
 module.exports = calculator;
