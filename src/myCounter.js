96900245ea7e8c4a32754707c96b15eaabb364c0
Augusto <augustocm2002@yahoo.com.br>
2021-05-29 15:16:43 -0300

    Fazendo requisito 8 - Alterando variavel para counter2

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,8 +20,8 @@ const myCounter = () => {
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     // eslint-disable-next-line no-shadow
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
