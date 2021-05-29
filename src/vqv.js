914810381e09149b5719cd5c63aeb2ca3d5abf0a
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-05-29 13:49:49 -0300

    concerta erro funcao VQV

--- src/vqv.js
+++ src/vqv.js
@@ -19,9 +19,9 @@
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) return undefined;
   return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
