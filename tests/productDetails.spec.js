4a13410d6128a2cfff380de570cf377c434a5997
Juliane Marques <mqs.juliane@gmail.com>
2021-05-29 17:33:00 -0300

    passsssoooooouuuuuuuu products

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -26,20 +26,18 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-const teste1 = Array.isArray(productDetails());
-const teste2 = productDetails('produto1', 'produto2');
-
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')));
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    const teste2 = productDetails('produto1', 'produto2');
     assert.strictEqual(Object.values(teste2[0]) !== Object.values(teste2[1]), true);
     // Teste que os dois productIds terminam com 123.
     assert.strictEqual(teste2[0].details.productId.substr(-3), '123');
