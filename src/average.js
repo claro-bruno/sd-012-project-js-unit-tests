e0ff0a1fb5e809fc854ce7d580b92650872da42f
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-29 12:34:35 -0300

    resolucao 1 req

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0 || typeof array !== 'object') {
+    return undefined;
+  }
+  let sum = 0;
+  for (let index = 0; index < array.length; i+=) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+  }
+  
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
