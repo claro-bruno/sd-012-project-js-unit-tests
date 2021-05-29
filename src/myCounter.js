bfa3f54742564e109fbada327477d26f42a4f3c4
Wadson <wadson.dev92@gmail.com>
2021-05-29 04:21:48 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,13 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let index = 2; index <= 3; index += 1) {
+      myArray.push(index);
     }
   }
   return myArray;
+
 };
+
 
 module.exports = myCounter;
