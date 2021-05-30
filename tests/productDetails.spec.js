28beee229ccf2a1a1982de255f7b9005a5a98b86
Victor Veloso <victorveloso13@gmail.com>
2021-05-29 22:59:04 -0300

    Requisito 6, agora vai!!

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -42,3 +42,9 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
       assert.strictEqual(typeof product, 'object');
     }
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productDetails('a', 'b')[0] !== productDetails('a', 'b')[1], true);
+    // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(products[0]['details']['productId'].endsWith('123'), true);
+    assert.strictEqual(products[1]['details']['productId'].endsWith('123'), true);
+  });
+});
