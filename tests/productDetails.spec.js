74392ac5be40dd9ede4d65d0910b92f604882028
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-05-30 18:13:38 -0300

    Adiciona teste que faltava requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -44,7 +44,9 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
 
     // Teste que os dois objetos são diferentes entre si.
     assert.notStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
+    assert.notStrictEqual(productDetails('Alcool gel', 'Máscara')[1], productDetails('Alcool gel', 'Máscara')[1]);
     
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123'), true);
   });
 });
