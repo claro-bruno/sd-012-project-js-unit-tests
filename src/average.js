661ee2bb2d852646d1f9fabaa619a0cab8c838d7
AndZoni <lorenzoniandre@outlook.com>
2021-05-30 15:13:50 +0100

    Adiciona Requisito 1 - Implemente a função average

--- src/average.js
+++ src/average.js
@@ -12,8 +12,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (numbers) => {
+  if (numbers.length === 0) return undefined;
+  let soma = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (typeof (numbers[i]) !== 'number') return undefined;
+    soma += numbers[i];
+  }
+  return Math.round(soma / numbers.length);
 };
 
 module.exports = average;
