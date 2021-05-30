1798b836b016b771f93a00bbad7d1960fa1fbb58
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-05-30 13:05:13 -0300

    Requisito 6.productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,16 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails(String, String), "object"); // array é um objeto
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.keys(productDetails()).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof Object.keys(productDetails()), "object");
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(Object.keys(productDetails()[0]) !== Object.keys(productDetails()[1]), true);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.endsWith('123') && productDetails()[1].details.productId.endsWith('123'), true); // Método .endWith() retirado da documetação ->> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
   });
 });
