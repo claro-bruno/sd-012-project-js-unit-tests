8e107679e2b29f25c969629b6e45d6387edaab5b
Mateus <mateusbgonzaga@gmail.com>
2021-05-30 16:44:01 -0300

    productDetails test refactoring

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,20 +28,21 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#productDetails', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+describe('6 - Implemente os casos de teste para a função productDetails', () => {
+  it('Verifica se a função productDetails tem o comportamento esperado', () => {
     //assert.fail();
-// ESCREVA SEUS TESTES ABAIXO:
+
+    // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
+    assert.strictEqual(typeof productDetails(), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual(Object.keys(productDetails('Alcool gel', 'Máscara')).length, 2);
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof Object.keys(productDetails('Alcool gel', 'Máscara')), 'object');
+    assert.deepStrictEqual(typeof productDetails(), 'object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notStrictEqual(Object.keys(productDetails('Alcool gel', 'Máscara'))[0], Object.keys(productDetails('Alcool gel', 'Máscara'))[1]);
-    // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123'), true);
-    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[1].details.productId.endsWith('123'), true);
+    assert.deepStrictEqual(productDetails('Um', 'Dois')[0] !== productDetails('Um', 'Dois')[1], true);
+    // Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(productDetails('Um', 'Dois')[0].details.productId === 'Um123', true);
+    assert.deepStrictEqual(productDetails('Um', 'Dois')[1].details.productId === 'Dois123', true);
   });
 });
