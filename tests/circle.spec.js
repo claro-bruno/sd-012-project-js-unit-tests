976c04013a7f2fca90d60f950cc9a2db6f2e4d53
Dock32 <brunocostah@hotmail.com>
2021-05-30 19:46:43 -0300

    Correcao requisito 4 - object

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,7 +35,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.strictEqual(circle(1) instanceof Object, true);
-    assert.strictEqual(object.keys(circle(1)).length, 3);
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
     assert.strictEqual(circle(3).radius, 3);
