7f495e6288eea2cb902262e8241ca60fa9907e81
gscuri <gscuri6@hotmail.com>
2021-05-30 17:19:50 -0300

    resolucao exercicio 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (studentName) => {
+  const studentObj = {
+    name: studentName,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return studentObj;
+};
 
 module.exports = createStudent;
