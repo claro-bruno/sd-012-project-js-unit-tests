008747c32aa53a701c572925eadd42d94b1d25a5
Gabriela-GUerra <gabrielaguerra.arq@gmail.com>
2021-05-29 21:45:02 -0300

    adiciona requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => ({
+   name: nome,
+   feedback: () => 'Eita pessoa boa!',
+});
+
+createStudent('Ana');
 
 module.exports = createStudent;
