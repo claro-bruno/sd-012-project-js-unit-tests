6b565018061876cbe47ba920336e5b845927c93e
Jossany Moura <mjossany@gmail.com>
2021-05-30 13:00:18 -0300

    Resolve conflito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -46,7 +46,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     };
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(arrayTeste[0], arrayTeste[1]);
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    // Teste que os dois productIds terminam com 123.
     assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123'), true)
     assert.strictEqual(productDetails('Alcool gel', 'Máscara')[1].details.productId.endsWith('123'), true)
   });
