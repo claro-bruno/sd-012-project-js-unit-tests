691661a77e0204eae139a5339808d764e116cf47
paulomichael <paulo.michael@gmail.com>
2021-05-30 20:56:43 -0300

    exercicio 9.2

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -60,8 +60,9 @@ describe('9 - Implemente os casos de teste e a função `createMenu`', () => {
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
-    // const objetoRetornado = createMenu({ food: {}, drink: {} });
-    // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
+    const objetoRetornado2 = createMenu({ food: {}, drink: {} });
+    objetoRetornado2.fetchMenu() // Retorno: { food: {}, drink: {}}
+    assert.deepStrictEqual(objetoRetornado2.fetchMenu(), {food: {}, drink: {} } )
     // ```
     // TESTE 3: Verifique que o menu passado pra função createMenu é identico ao menu recuperado pela função 'objetoRetornado.fetchMenu'
     // ```
