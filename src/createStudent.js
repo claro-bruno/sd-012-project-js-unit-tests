9c4c6935a2357daa30a49e948ab12927555ef210
Gutemberg <gttj2010@hotmail.com>
2021-05-29 23:08:12 -0300

    Requisito 5: Refatoraçao 1 da Funçao CreateStudent.

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,7 +16,7 @@
 */
 
 const createStudent = (nome) => {  
-  const estudante = {name: nome, feedback: () => 'Eita pessoa boa!'};
+  const estudante = { name: nome, feedback: () => 'Eita pessoa boa!' };
   return estudante;
 };
 
