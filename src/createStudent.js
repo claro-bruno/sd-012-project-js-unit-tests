a9bd70d17dcefbfd933500689f79e679d8284b58
Gutemberg <gttj2010@hotmail.com>
2021-05-29 23:31:33 -0300

    Requisito 6: Funçao ProductDetails.

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,7 +15,7 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {  
+const createStudent = (nome) => {
   const estudante = { name: nome, feedback: () => 'Eita pessoa boa!' };
   return estudante;
 };
