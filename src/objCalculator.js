7f407b414c002d729423cc98b25adedb09c33452
Gabriela-GUerra <gabrielaguerra.arq@gmail.com>
2021-05-29 22:15:24 -0300

    adiciona requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,17 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (a, b) => a + b, 
+  mult: (a, b) => a * b, 
+  div: (a, b) => { 
+    return Math.floor(a / b)
+  },
+  sub: (a, b) => a - b, 
+};
+calculator.add(2, 5);
+calculator.mult(2, 5);
+calculator.div(2, 5);
+calculator.sub(2, 5);
 
 module.exports = calculator;
