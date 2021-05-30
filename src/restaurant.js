89fd4298224982eff0f1c1b3bb52825220b134ab
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-05-30 16:09:42 -0300

    fim do fim

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,12 +79,4 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = (obj) => {
-  return ({
-    fetchMenu: () => obj,
-    consumption: [],
-
-  });
-};
-
 module.exports = createMenu;
