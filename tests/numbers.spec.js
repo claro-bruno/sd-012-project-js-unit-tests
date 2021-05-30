35327fc949eda85d9bb90297b3803085e38c783b
Mateus <mateusbgonzaga@gmail.com>
2021-05-30 15:00:01 -0300

    second test concluded

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,12 +16,16 @@ const numbers = require('../src/numbers');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('2 - Implemente os casos de teste para a função `numbers`', () => {
+describe('#numbers', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
-    assert.fail();
+    //assert.fail();
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    assert.strictEqual(numbers([1, 2, 3, 4, 5]),true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.strictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.strictEqual(numbers(['']), false);
   });
 });
