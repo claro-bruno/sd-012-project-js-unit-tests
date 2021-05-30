a5c1e0e663b8bc3e228c0967b5bf1eacac3bd274
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-05-30 13:59:18 -0300

    correcao req 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -19,11 +19,11 @@ const numbers = require('../src/numbers');
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    assert.deepStrictEqual(numbers([1, 2, 3, 4, 5], true))
+    assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true)
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5], false))
+    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false)
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    assert.deepStrictEqual(numbers([1, 'a', 3], false))
+    assert.deepStrictEqual(numbers([1, 'a', 3]), false)
     // Escreva um teste em que a função recebe [' '] e retorna false
     assert.deepStrictEqual(numbers([' ']), false)
   });
