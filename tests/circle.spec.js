17922a634b951322da845553114b109814a9f02f
Dock32 <brunocostah@hotmail.com>
2021-05-30 19:36:01 -0300

    Correcao requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -38,6 +38,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.strictEqual(object.keys(circle(1)).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
-    assert.strictEqual(circle(3), {'area': 28.259999999999998, circumference: 18.84});
+    assert.strictEqual(circle(3).radius, 3);
+    assert.deepStrictEqual(circle(3), {'area': 28.259999999999998, circumference: 18.84});
   });
 });
