dc233a329e6977d9e31cde03101c852fa1f881a4
Lucas Basques <lukasbasques@gmail.com>
2021-05-29 21:59:31 -0300

    Requisito 8, correçao lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -25,14 +25,3 @@ const myCounter = () => {
   }
   return myArray;
 };
-
-module.exports = myCounter;
-
-var myArray = [];
-for (var counter = 0; counter <= 3; counter += 1) {
-  myArray.push(counter);
-  for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
-    myArray.push(counter2);
-  }
-}
-return myArray;
