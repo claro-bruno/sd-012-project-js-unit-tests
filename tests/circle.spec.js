a01f2a4d88d9c357e4c3d70d65e26682cb4d63f8
Dock32 <brunocostah@hotmail.com>
2021-05-30 19:55:25 -0300

    Correcao requisito 4 - acrescentando radius na linha 42

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,7 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    //assert.strt
+    assert.strt
     //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
@@ -39,6 +39,6 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
     assert.strictEqual(circle(3).radius, 3);
-    assert.deepStrictEqual(circle(3), {'area': 28.259999999999998, circumference: 18.84});
+    assert.deepStrictEqual(circle(3), {'area': 28.259999999999998, 'circumference': 18.84, 'radius': 3});
   });
 });
