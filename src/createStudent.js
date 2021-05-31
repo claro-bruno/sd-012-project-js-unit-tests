b3eee4130961b69525207c2b136e4897e7d46885
Igor.Duarte <igorhdsouza@gmail.com>
2021-05-31 09:57:22 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (parametro) => {
+  const student = {
+    name: parametro,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
 
 module.exports = createStudent;
