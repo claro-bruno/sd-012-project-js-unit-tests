bb02c7e015d2ae6446413968d2b53446cf3871a2
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-31 16:08:36 -0300

    refatora 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
+const createStudent = (nome) => {
   const student = {
     name: nome,
      feedback: () =>'Eita pessoa boa!'
-   }
+   };
   return student;
   };
 
