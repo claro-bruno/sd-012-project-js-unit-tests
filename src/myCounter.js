65b1cee01c7e9e5e000fb238c11548c9adb1fae5
Igor.Duarte <igorhdsouza@gmail.com>
2021-05-31 11:50:22 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,15 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let contador = 2; contador <= 3; contador += 1) {
+      myArray.push(contador);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
