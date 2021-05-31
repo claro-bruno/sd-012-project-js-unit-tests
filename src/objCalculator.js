dde2745931e08a22deb113f3dd497e41791210ad
Dock32 <brunocostah@hotmail.com>
2021-05-31 02:32:15 -0300

    Requisito 7 corrigindo posisao das funcao

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -21,8 +21,8 @@
 
 const calculator = {
   add: (num1, num2) => Math.floor(num1 + num2),
-  vid: (num1, num2) => Math.floor(num1 / num2),
   mult: (num1, num2) => Math.floor(num1 * num2),
+  div: (num1, num2) => Math.floor(num1 / num2),
   sub: (num1, num2) => Math.floor(num1 - num2),
 };
 
