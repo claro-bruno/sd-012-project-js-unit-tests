d72953fb13aea345c72bd5112f27b03b052f6237
AndZoni <lorenzoniandre@outlook.com>
2021-05-31 01:16:16 +0100

    Corrige erro Conflicting files

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,11 +27,12 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.strictEqual(Array.isArray(productDetails()), true);
-    assert.strictEqual(productDetails().length, 2);
-    assert.strictEqual(typeof productDetails()[0] && typeof productDetails()[1], 'object');
-    assert.notStrictEqual(productDetails()[0], productDetails()[1]);
-    assert.strictEqual(productDetails()[0].details.productId.endsWith('123'), true);
-    assert.strictEqual(productDetails()[1].details.productId.endsWith('123'), true);
+    assert.fail();
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    // Teste que o array retornado pela função contém dois itens dentro.
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    // Teste que os dois objetos são diferentes entre si.
+    // Teste que os dois productIds terminam com 123.
   });
 });
