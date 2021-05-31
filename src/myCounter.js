01414703ee244deadc780680ab7f4d6d1a1c98d8
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-31 19:08:51 -0300

    Requirement 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,10 +17,10 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let count = 2; count <= 3; count += 1) {
+      myArray.push(count);
     }
   }
   return myArray;
