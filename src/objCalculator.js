96963083d4da56e5f54ad75e8cbee9dbeb1d6c69
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-31 20:20:21 -0300

    corrige lint e revisa 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,10 +20,10 @@
 */
 
 const calculator = {
-  add : (num1,num2) => num1 + num2,
-  mult : (num1,num2) => num1 * num2,
-  div : (num1,num2) => Math.floor(num1 / num2),
-  sub : (num1,num2) => num1 - num2,
+  add: (num1, num2) => num1 + num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => Math.floor(num1 / num2),
+  sub: (num1, num2) => num1 - num2,
 };
 
 module.exports = calculator;
