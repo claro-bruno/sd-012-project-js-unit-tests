f22b1b451bf5f06e4bcf00e8c28538404913d05b
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-31 16:24:55 -0300

    corrige lint 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,12 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  const student = {
-    name: nome,
-     feedback: () =>'Eita pessoa boa!'
-   };
-  return student;
-  };
-
+const createStudent = (name) => ({
+    name,
+    feedback: () => 'Eita pessoa boa!',
+   });
+  
 module.exports = createStudent;
