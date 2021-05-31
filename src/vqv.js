4f174ad7fd2660c4088f975043dced8f8bec381f
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-31 14:48:59 -0300

    lint 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,8 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === undefined || idade === undefined) return undefined;
-  return `Oi, meu nome é ${nome}!Tenho ${idade} anos,
+  if (nome === undefined || idade === undefined) {
+   return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
 };
