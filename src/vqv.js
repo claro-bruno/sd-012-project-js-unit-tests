1aafee361acec3662e890d3c8a2aae5151bf1a7c
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-31 11:05:52 -0300

    Requirement 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if ( nome === null || idade === null) {
+    return undefined;
+} { `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+
+}
+  
+};
 
 module.exports = vqv;
