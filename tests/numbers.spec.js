9ddb6166d2d124b7ab14d436a48bdfa3496c9b83
LuctReis <luctreis@gmail.com>
2021-05-31 09:43:24 -0300

    Erro de digitacao requisito 2 corrigido

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -19,7 +19,7 @@ const numbers = require('../src/numbers');
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna um array e se o array retornado contém somente números', () => {
     //assert.fail();
-    assert,deeoStrictEqual(numbers([1,2,3,4,5]), true);
+    assert.deepStrictEqual(numbers([1,2,3,4,5]), true);
     assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
     assert.deepStrictEqual(numbers([1, 'a', 3]), false);
     assert.deepStrictEqual(numbers([' ']), false);
