9e89c6166a08c20d32e2bac78ffe854b95839e90
LuctReis <luctreis@gmail.com>
2021-05-31 10:37:22 -0300

    Verifica requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,7 +20,7 @@ const myCounter = () => {
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+      myArray.push(counter2);
     }
   }
   return myArray;
