0d205d519351bc7170fbca65ef12ee1ef059c34a
LuctReis <luctreis@gmail.com>
2021-05-31 10:18:08 -0300

    Corrigindo Requisito 4

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,7 +15,7 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {
+const createStudent = (nome) => {
   const student = {
     name: nome,
     feedback: () => {
