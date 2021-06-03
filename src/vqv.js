e3de03deb495cd0b47794c8339d0e0aa6da15a4f
Darciano Utzig <darciano@utzig.com.br>
2021-06-03 11:25:35 -0300

    Resolve lint requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -19,7 +19,7 @@
 const vqv = (nome, idade) => {
   if (typeof nome !== 'string' || typeof idade !== 'number') {
     return undefined;
-  };
+  }
 
   return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
