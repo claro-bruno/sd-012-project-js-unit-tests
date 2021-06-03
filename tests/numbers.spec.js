3b8dcc612c1e25e2ec7f44c6d38f5e31f21d9fa1
Aldria Melo <aldriamelo.academico@gmail.com>
2021-06-03 17:27:59 -0300

    altera requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -21,6 +21,6 @@ describe('2 - Implemente os casos de teste para a função `numbers`', () => {
     assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
     assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
     assert.strictEqual(numbers([1, 'a', 3]), false);
-    assert.strictEqual(numbers([' '], false);
+    assert.strictEqual(numbers([' ']), false);
   });
 });
