c1176c1695ef7840386cecf0d04f47aab0b0ece4
Darciano Utzig <darciano@utzig.com.br>
2021-06-03 11:23:14 -0300

    Resolve requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof nome !== 'string' || typeof idade !== 'number') {
+    return undefined;
+  };
+
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+};
+
+// console.log(vqv('Tonico', 30))
 
 module.exports = vqv;
