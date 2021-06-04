67cb0c5f49fa78626d561f46cc62eac902b65f39
PeDD1 <pedrohr0909@gmail.com>
2021-06-04 11:29:41 -0300

    Requisito 3 realizado com lint

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (!nome && !idade) {
+    return undefined;
+  }
+  const string = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return string;
+};
 
 module.exports = vqv;
