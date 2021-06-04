295734311750629e45ebc7eff9d3d4e058c7d88c
PeDD1 <pedrohr0909@gmail.com>
2021-06-04 11:35:26 -0300

    Requisito 4 realizado com lint 'corrigido'

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,6 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
      // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle(3), 'object')
