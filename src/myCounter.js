e2051be61fb2bb15573806e4260f4f16cf978492
Xenon-Neko <darkankher@gmail.com>
2021-06-16 13:35:54 -0300

    projeto unit tests, corrigindo espaço

--- src/myCounter.js
+++ src/myCounter.js
@@ -1,4 +1,3 @@
-
 /*
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
   Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
